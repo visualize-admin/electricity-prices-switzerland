ce3f099abf9620ea8b29de3bd7aa6d9467494ea5
Luc <luc.guillemot@gmail.com>
2020-06-17 17:49:07 +0200

    fix: typescript

--- src/docs/layout.docs.tsx
+++ src/docs/layout.docs.tsx
@@ -7,19 +7,20 @@ export default () => markdown`
 ### Spacings
 The application uses a limited set of space-variables, to construct the user interface structure. This allows for the great flexibility, while allowing to define relationships between elements on a global level. The system of global space-variables is documented below:
 
-${(
-  <TableSpecimen
-    rows={theme.space.map((s, i) => ({
-      Variable: `space-${i}`,
-      Measurement: s,
-      Space: <Space width={s} />,
-    }))}
-    span={4}
-  />
-)}
-
+${
+  Array.isArray(theme.space) ? (
+    <TableSpecimen
+      rows={theme.space.map((s, i) => ({
+        Variable: `space-${i}`,
+        Measurement: s,
+        Space: <Space width={s} />,
+      }))}
+      span={4}
+    />
+  ) : null
+}
 `;
 
-const Space = ({ width }: { width: string }) => (
-  <Box sx={{ width, height: "2rem", bg: "darkgrey" }} />
+const Space = ({ width }: { width: string | number }) => (
+  <Box sx={{ width, height: "2rem", bg: "brand" }} />
 );
