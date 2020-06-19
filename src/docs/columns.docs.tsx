2069d853a4961c41a87bb6d7ffb67e8c705dd66c
Luc <luc.guillemot@gmail.com>
2020-06-19 10:16:14 +0200

    docs: stacked columns y label

--- src/docs/columns.docs.tsx
+++ src/docs/columns.docs.tsx
@@ -140,7 +140,7 @@ export default () => {
         measures={[
           {
             iri: "value",
-            label: "value",
+            label: "Price (CHF)",
             __typename: "Measure",
           },
         ]}
