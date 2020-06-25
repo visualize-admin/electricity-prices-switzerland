10b76b6889d51012892c902aabd1e2bf9ca7335b
Luc <luc.guillemot@gmail.com>
2020-06-25 17:24:27 +0200

    docs: use elCom related examples

--- src/docs/combobox.docs.tsx
+++ src/docs/combobox.docs.tsx
@@ -2,13 +2,28 @@ import { markdown, ReactSpecimen } from "catalog";
 import { ComboboxMulti, Combobox } from "../components/combobox";
 
 export default () => markdown`
-> 
+>
 
 ## Combobox (single select)
 
   ${(
     <ReactSpecimen>
-      <Combobox label="Choose something" items={["apple", "banana", "cherry", "date", "asparagus"]} />
+      <Combobox
+        label="Choose something"
+        items={[
+          "AEK Energie AG",
+          "Aare Versorgungs AG",
+          "Azienda Elettrica Bregaglia AEB",
+          "Werkbetriebe der Dorfgemeinde Matzingen",
+          "Werke am Zürichsee (Küsnacht)",
+          "Werke am Zürichsee (Erlenbach)",
+          "Werke am Zürichsee (Zollikon)",
+          "Romande Energie SA",
+          "Energie Wasser Bern ewb",
+          "Services Industriels de Genève SIG",
+          "ewz",
+        ]}
+      />
     </ReactSpecimen>
   )}
 
@@ -16,7 +31,22 @@ export default () => markdown`
 
   ${(
     <ReactSpecimen>
-      <ComboboxMulti label="Choose something" items={["apple", "banana", "cherry", "date", "asparagus"]} />
+      <ComboboxMulti
+        label="Choose something"
+        items={[
+          "AEK Energie AG",
+          "Aare Versorgungs AG",
+          "Azienda Elettrica Bregaglia AEB",
+          "Werkbetriebe der Dorfgemeinde Matzingen",
+          "Werke am Zürichsee (Küsnacht)",
+          "Werke am Zürichsee (Erlenbach)",
+          "Werke am Zürichsee (Zollikon)",
+          "Romande Energie SA",
+          "Energie Wasser Bern ewb",
+          "Services Industriels de Genève SIG",
+          "ewz",
+        ]}
+      />
     </ReactSpecimen>
   )}
 
