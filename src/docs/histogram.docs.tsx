bae99e374d865620c3004e3daf389f873239f26f
Luc <luc.guillemot@gmail.com>
2020-06-25 15:52:42 +0200

    docs: histogram

---
+++ src/docs/histogram.docs.tsx
@@ -0,0 +1,63 @@
+import { markdown, ReactSpecimen } from "catalog";
+import { Tooltip } from "../components/charts-generic/annotations/tooltip";
+import { AxisHeightLinear } from "../components/charts-generic/axis/axis-height-linear";
+import {
+  AxisWidthBand,
+  AxisWidthBandDomain,
+} from "../components/charts-generic/axis/axis-width-band";
+import { Columns } from "../components/charts-generic/columns/columns-simple";
+import { ColumnChart } from "../components/charts-generic/columns/columns-state";
+import {
+  ChartContainer,
+  ChartSvg,
+} from "../components/charts-generic/containers";
+import { InteractionColumns } from "../components/charts-generic/interaction/interaction-columns";
+import { pivot_longer } from "../domain/helpers";
+import { zurichAndGeneva } from "./fixtures";
+import { standard2020 } from "./data/standard-2020";
+import { HistogramColumns } from "../components/charts-generic/histogram/histogram";
+import { Histogram } from "../components/charts-generic/histogram/histogram-state";
+export default () => {
+  const data = standard2020.filter(
+    (d) => d.Jahr === "2020" && d.Kategorie === "H4"
+  );
+  console.table(data);
+  console.log(data.length);
+
+  return markdown`
+> Histogram
+
+  ${(
+    <ReactSpecimen span={6}>
+      <Histogram
+        data={data}
+        fields={{
+          x: {
+            componentIri: "Total exkl. MWST",
+          },
+        }}
+        measures={[
+          {
+            iri: "Total exkl. MWST",
+            label: "Total exkl. MWST",
+            __typename: "Measure",
+          },
+        ]}
+        aspectRatio={0.4}
+      >
+        <ChartContainer>
+          <ChartSvg>
+            {/* <AxisHeightLinear /> <AxisWidthBand />
+            <Columns /> <AxisWidthBandDomain />
+            <InteractionColumns /> */}
+            <HistogramColumns />
+          </ChartSvg>
+          <Tooltip type="single" />
+        </ChartContainer>
+      </Histogram>
+    </ReactSpecimen>
+  )}
+
+
+  `;
+};
