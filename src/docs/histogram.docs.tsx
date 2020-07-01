c174939e01ca3c35a1cdb77986b3d432e793772b
Luc <luc.guillemot@gmail.com>
2020-07-01 17:42:07 +0200

    docs: histogram annotations

--- src/docs/histogram.docs.tsx
+++ src/docs/histogram.docs.tsx
@@ -9,7 +9,10 @@ import {
   ChartContainer,
   ChartSvg,
 } from "../components/charts-generic/containers";
-import { HistogramColumns } from "../components/charts-generic/histogram/histogram";
+import {
+  HistogramColumns,
+  HistogramAnnotation,
+} from "../components/charts-generic/histogram/histogram";
 import { Histogram } from "../components/charts-generic/histogram/histogram-state";
 import { standardH12020 } from "./data/2020-standard-H1";
 import { Median } from "../components/charts-generic/histogram/median";
@@ -17,41 +20,112 @@ import {
   AxisWidthHistogram,
   AxisWidthHistogramDomain,
 } from "../components/charts-generic/axis/axis-width-histogram";
+import { AnnotationProvider } from "../components/charts-generic/use-annotation";
 
 export default () => {
   return markdown`
-> Histogram
+## Base Histogram
 
   ${(
     <ReactSpecimen span={6}>
-      <Histogram
-        data={standardH12020}
-        fields={{
-          x: {
-            componentIri: "Total exkl. MWST",
-          },
-        }}
-        measures={[
+      <AnnotationProvider d={undefined}>
+        <Histogram
+          data={standardH12020}
+          fields={{
+            x: {
+              componentIri: "Total exkl. MWST",
+            },
+          }}
+          measures={[
+            {
+              iri: "Total exkl. MWST",
+              label: "Total exkl. MWST",
+              __typename: "Measure",
+            },
+          ]}
+          aspectRatio={0.4}
+        >
+          <ChartContainer>
+            <ChartSvg>
+              <AxisHeightLinear />
+              <AxisHeightLinearDomain />
+              <AxisWidthHistogram />
+              <AxisWidthHistogramDomain />
+              <HistogramAnnotation />
+              <HistogramColumns />
+              <Median label="CH Median" />
+            </ChartSvg>
+            <Tooltip type="single" />
+          </ChartContainer>
+        </Histogram>
+      </AnnotationProvider>
+    </ReactSpecimen>
+  )}
+
+  ## With annotations
+
+  ${(
+    <ReactSpecimen span={6}>
+      <AnnotationProvider
+        d={[
           {
-            iri: "Total exkl. MWST",
-            label: "Total exkl. MWST",
-            __typename: "Measure",
+            ID: 3,
+            Netzbetreiber: "Administration Communale Courchapoix",
+            "VSE-ID": "10819012345",
+            Kategorie: "H1",
+            Netznutzung: 17.0975,
+            Energie: 8.6,
+            Abgabe: 1.2,
+            KEV: 2.3,
+            "Total exkl. MWST": 29.1975,
+            Jahr: "2020",
+            Produkt: "standard",
+          },
+          {
+            ID: 757,
+            Netzbetreiber: "Technische Betriebe Weinfelden AG",
+            "VSE-ID": "10269012345",
+            Kategorie: "H1",
+            Netznutzung: 15.9256,
+            Energie: 7.6197,
+            Abgabe: 0,
+            KEV: 2.3,
+            "Total exkl. MWST": 25.8453,
+            Jahr: "2020",
+            Produkt: "standard",
           },
         ]}
-        aspectRatio={0.4}
       >
-        <ChartContainer>
-          <ChartSvg>
-            <AxisHeightLinear />
-            <AxisHeightLinearDomain />
-            <AxisWidthHistogram />
-            <AxisWidthHistogramDomain />
-            <HistogramColumns />
-            <Median label="CH Median" />
-          </ChartSvg>
-          <Tooltip type="single" />
-        </ChartContainer>
-      </Histogram>
+        <Histogram
+          data={standardH12020}
+          fields={{
+            x: {
+              componentIri: "Total exkl. MWST",
+            },
+          }}
+          measures={[
+            {
+              iri: "Total exkl. MWST",
+              label: "Total exkl. MWST",
+              __typename: "Measure",
+            },
+          ]}
+          aspectRatio={0.4}
+        >
+          <ChartContainer>
+            <ChartSvg>
+              <AxisHeightLinear />
+              <AxisHeightLinearDomain />
+              <AxisWidthHistogram />
+              <AxisWidthHistogramDomain />
+              <HistogramAnnotation />
+              <HistogramColumns />
+              <Median label="CH Median" />
+            </ChartSvg>
+            <Tooltip type="single" />
+          </ChartContainer>
+        </Histogram>
+      </AnnotationProvider>
     </ReactSpecimen>
   )}
   `;
