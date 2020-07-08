d3c1787e1638e2eb34613b1669479e5060763720
Luc <luc.guillemot@gmail.com>
2020-07-08 14:42:10 +0200

    fix: Combobox documentation

--- src/docs/combobox.docs.tsx
+++ src/docs/combobox.docs.tsx
@@ -1,5 +1,6 @@
 import { markdown, ReactSpecimen } from "catalog";
 import { ComboboxMulti, Combobox } from "../components/combobox";
+import { useState } from "react";
 
 export default () => markdown`
 >
@@ -8,22 +9,7 @@ export default () => markdown`
 
   ${(
     <ReactSpecimen>
-      <Combobox
-        label="Choose something"
-        items={[
-          "AEK Energie AG",
-          "Aare Versorgungs AG",
-          "Azienda Elettrica Bregaglia AEB",
-          "Werkbetriebe der Dorfgemeinde Matzingen",
-          "Werke am Zürichsee (Küsnacht)",
-          "Werke am Zürichsee (Erlenbach)",
-          "Werke am Zürichsee (Zollikon)",
-          "Romande Energie SA",
-          "Energie Wasser Bern ewb",
-          "Services Industriels de Genève SIG",
-          "ewz",
-        ]}
-      />
+      <ComboboxExample />
     </ReactSpecimen>
   )}
 
@@ -51,3 +37,29 @@ export default () => markdown`
   )}
 
 `;
+
+const ComboboxExample = () => {
+  const [selectedItem, update] = useState("Werke am Zürichsee (Zollikon)");
+  const handleSelectedItemChange = ({ selectedItem }: any) =>
+    update(selectedItem);
+  return (
+    <Combobox
+      label="Choose something"
+      items={[
+        "AEK Energie AG",
+        "Aare Versorgungs AG",
+        "Azienda Elettrica Bregaglia AEB",
+        "Werkbetriebe der Dorfgemeinde Matzingen",
+        "Werke am Zürichsee (Küsnacht)",
+        "Werke am Zürichsee (Erlenbach)",
+        "Werke am Zürichsee (Zollikon)",
+        "Romande Energie SA",
+        "Energie Wasser Bern ewb",
+        "Services Industriels de Genève SIG",
+        "ewz",
+      ]}
+      selectedItem={selectedItem}
+      handleSelectedItemChange={handleSelectedItemChange}
+    />
+  );
+};
