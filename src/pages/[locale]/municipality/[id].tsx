f77a5ad1d344f21c8d7239dd31b2021306668f12
Luc <luc.guillemot@gmail.com>
2020-07-15 15:26:22 +0200

    feat: get municipality data

--- src/pages/[locale]/municipality/[id].tsx
+++ src/pages/[locale]/municipality/[id].tsx
@@ -5,11 +5,38 @@ import { Header } from "../../../components/header";
 import { Trans } from "@lingui/macro";
 import { LocalizedLink } from "../../../components/links";
 import { Link as UILink } from "theme-ui";
+import { useObservationsQuery, PriceComponent } from "../../../graphql/queries";
+import * as React from "react";
+
+const EMPTY_ARRAY: never[] = [];
 
 const MunicipalityPage = () => {
-  const { id } = useRouter().query;
+  const { query } = useRouter();
+
   const kantonId = "261";
   const providerIds = ["xxx", "yyy"];
+
+  const priceComponent =
+    (query.priceComponent as PriceComponent) ?? PriceComponent.Total; // TODO: parameterize priceComponent
+  const category = query.category as string;
+
+  const [observationsQuery] = useObservationsQuery({
+    variables: {
+      priceComponent,
+      filters: {
+        period: ["2020"],
+        category: [
+          `https://energy.ld.admin.ch/elcom/energy-pricing/category/H1`,
+        ],
+      },
+    },
+  });
+  const observations = observationsQuery.fetching
+    ? EMPTY_ARRAY
+    : observationsQuery.data?.cubeByIri?.observations ?? EMPTY_ARRAY;
+
+  console.log({ observations });
+
   return (
     <Flex sx={{ minHeight: "100vh", flexDirection: "column" }}>
       <Header></Header>
@@ -24,7 +51,7 @@ const MunicipalityPage = () => {
         <Box sx={{ px: 4, py: 6, bg: "monochrome100" }}>
           <Box sx={{ maxWidth: "65rem", mx: "auto", my: 2 }}>
             <Text as="h1" variant="heading1" sx={{ color: "monochrome800" }}>
-              Municipality {id}
+              Municipality {query.id}
             </Text>
 
             <Flex sx={{ flexWrap: "wrap" }}>
@@ -41,7 +68,7 @@ const MunicipalityPage = () => {
               <Box sx={{ pr: 3, my: 1 }}>
                 <Trans id="detail.municipality">Netzbetrieber</Trans>:{" "}
                 {providerIds.map((providerId, i) => (
-                  <>
+                  <React.Fragment key={i}>
                     <LocalizedLink
                       pathname="/[locale]/provider/[id]"
                       query={{ id: providerId }}
@@ -50,7 +77,7 @@ const MunicipalityPage = () => {
                       <UILink variant="inline">{providerId}</UILink>
                     </LocalizedLink>
                     {i < providerIds.length - 1 && ", "}
-                  </>
+                  </React.Fragment>
                 ))}
               </Box>
             </Flex>
