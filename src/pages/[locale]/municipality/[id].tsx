452de290bbbb3a4a25c9d2bf8078376a2b865e50
Luc <luc.guillemot@gmail.com>
2020-07-15 11:43:20 +0200

    feat: link kanton and provider from muni page

--- src/pages/[locale]/municipality/[id].tsx
+++ src/pages/[locale]/municipality/[id].tsx
@@ -3,10 +3,13 @@ import { useRouter } from "next/router";
 import { Footer } from "../../../components/footer";
 import { Header } from "../../../components/header";
 import { Trans } from "@lingui/macro";
+import { LocalizedLink } from "../../../components/links";
+import { Link as UILink } from "theme-ui";
 
 const MunicipalityPage = () => {
   const { id } = useRouter().query;
-
+  const kantonId = "261";
+  const providerIds = ["xxx", "yyy"];
   return (
     <Flex sx={{ minHeight: "100vh", flexDirection: "column" }}>
       <Header></Header>
@@ -23,18 +26,32 @@ const MunicipalityPage = () => {
             <Text as="h1" variant="heading1" sx={{ color: "monochrome800" }}>
               Municipality {id}
             </Text>
+
             <Flex sx={{ flexWrap: "wrap" }}>
               <Box sx={{ pr: 3, my: 1 }}>
                 <Trans id="detail.canton">Kanton</Trans>:{" "}
-                <Box as="span" sx={{ color: "primary" }}>
-                  KantonName
-                </Box>
+                <LocalizedLink
+                  pathname="/[locale]/canton/[id]"
+                  query={{ id: kantonId }}
+                  passHref
+                >
+                  <UILink variant="inline">Kanton</UILink>
+                </LocalizedLink>
               </Box>
               <Box sx={{ pr: 3, my: 1 }}>
-                <Trans id="detail.municipality">Gemeinde</Trans>:{" "}
-                <Box as="span" sx={{ color: "primary" }}>
-                  Gemeinde Name(s)
-                </Box>
+                <Trans id="detail.municipality">Netzbetrieber</Trans>:{" "}
+                {providerIds.map((providerId, i) => (
+                  <>
+                    <LocalizedLink
+                      pathname="/[locale]/provider/[id]"
+                      query={{ id: providerId }}
+                      passHref
+                    >
+                      <UILink variant="inline">{providerId}</UILink>
+                    </LocalizedLink>
+                    {i < providerIds.length - 1 && ", "}
+                  </>
+                ))}
               </Box>
             </Flex>
           </Box>
