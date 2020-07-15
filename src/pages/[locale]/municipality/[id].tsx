460ca1b9ff95e430d960c3f7b91d9de4e2cbfdd9
Luc <luc.guillemot@gmail.com>
2020-07-15 16:19:59 +0200

    feat: add bar and line chart

--- src/pages/[locale]/municipality/[id].tsx
+++ src/pages/[locale]/municipality/[id].tsx
@@ -7,6 +7,25 @@ import { LocalizedLink } from "../../../components/links";
 import { Link as UILink } from "theme-ui";
 import { useObservationsQuery, PriceComponent } from "../../../graphql/queries";
 import * as React from "react";
+import { DetailPageBanner } from "../../../components/detail-page/detail-page-banner";
+import { BarChart } from "../../../components/charts-generic/bars/bars-state";
+import {
+  ChartContainer,
+  ChartSvg,
+} from "../../../components/charts-generic/containers";
+import { Bars } from "../../../components/charts-generic/bars/bars-simple";
+import { AxisHeightLinear } from "../../../components/charts-generic/axis/axis-height-linear";
+import { LineChart } from "../../../components/charts-generic/lines/lines-state";
+import {
+  AxisTime,
+  AxisTimeDomain,
+} from "../../../components/charts-generic/axis/axis-width-time";
+import { Lines } from "../../../components/charts-generic/lines/lines";
+import { InteractionHorizontal } from "../../../components/charts-generic/overlay/interaction-horizontal";
+import { Ruler } from "../../../components/charts-generic/interaction/ruler";
+import { HoverDotMultiple } from "../../../components/charts-generic/interaction/hover-dots-multiple";
+import { Tooltip } from "../../../components/charts-generic/interaction/tooltip";
+import { LegendColor } from "../../../components/charts-generic/legends/color";
 
 const EMPTY_ARRAY: never[] = [];
 
@@ -16,17 +35,20 @@ const MunicipalityPage = () => {
   const kantonId = "261";
   const providerIds = ["xxx", "yyy"];
 
-  const priceComponent =
-    (query.priceComponent as PriceComponent) ?? PriceComponent.Total; // TODO: parameterize priceComponent
-  const category = query.category as string;
+  const municipalityId = query.id;
+
+  const priceComponent = PriceComponent.Total; // TODO: parameterize priceComponent
+  const category = (query.category as string) ?? "H4";
 
   const [observationsQuery] = useObservationsQuery({
     variables: {
       priceComponent,
       filters: {
-        period: ["2020"],
         category: [
-          `https://energy.ld.admin.ch/elcom/energy-pricing/category/H1`,
+          `https://energy.ld.admin.ch/elcom/energy-pricing/category/${category}`,
+        ],
+        municipality: [
+          `http://classifications.data.admin.ch/municipality/${query.id}`,
         ],
       },
     },
@@ -35,7 +57,8 @@ const MunicipalityPage = () => {
     ? EMPTY_ARRAY
     : observationsQuery.data?.cubeByIri?.observations ?? EMPTY_ARRAY;
 
-  console.log({ observations });
+  console.log({ municipalityId });
+  console.table(observations);
 
   return (
     <Flex sx={{ minHeight: "100vh", flexDirection: "column" }}>
@@ -48,39 +71,99 @@ const MunicipalityPage = () => {
           flexDirection: "column",
         }}
       >
-        <Box sx={{ px: 4, py: 6, bg: "monochrome100" }}>
-          <Box sx={{ maxWidth: "65rem", mx: "auto", my: 2 }}>
-            <Text as="h1" variant="heading1" sx={{ color: "monochrome800" }}>
-              Municipality {query.id}
-            </Text>
+        <DetailPageBanner
+          entity={municipalityId as string}
+          kanton={"kanton"}
+          linkedIds={["xxx", "yyy"]}
+        />
 
-            <Flex sx={{ flexWrap: "wrap" }}>
-              <Box sx={{ pr: 3, my: 1 }}>
-                <Trans id="detail.canton">Kanton</Trans>:{" "}
-                <LocalizedLink
-                  pathname="/[locale]/canton/[id]"
-                  query={{ id: kantonId }}
-                  passHref
-                >
-                  <UILink variant="inline">Kanton</UILink>
-                </LocalizedLink>
-              </Box>
-              <Box sx={{ pr: 3, my: 1 }}>
-                <Trans id="detail.municipality">Netzbetrieber</Trans>:{" "}
-                {providerIds.map((providerId, i) => (
-                  <React.Fragment key={i}>
-                    <LocalizedLink
-                      pathname="/[locale]/provider/[id]"
-                      query={{ id: providerId }}
-                      passHref
-                    >
-                      <UILink variant="inline">{providerId}</UILink>
-                    </LocalizedLink>
-                    {i < providerIds.length - 1 && ", "}
-                  </React.Fragment>
-                ))}
-              </Box>
-            </Flex>
+        <Box sx={{ width: "100%", maxWidth: "65rem", mx: "auto", my: 2 }}>
+          {/* BAR CHART */}
+          <Box sx={{ bg: "monochrome100", p: 5, m: 4 }}>
+            <Text as="h2" variant="heading2" sx={{ pt: 1 }}>
+              <Trans id="detail.card.price.components">Preiskomponenten</Trans>
+            </Text>
+            <BarChart
+              data={observations
+                .filter((obs) => obs.period === "2018")
+                .map((obs) => ({
+                  priceComponent: "Total (exkl. MwSt.)",
+                  ...obs,
+                }))}
+              fields={{
+                x: {
+                  componentIri: "value",
+                },
+                y: {
+                  componentIri: "priceComponent",
+                  sorting: { sortingType: "byMeasure", sortingOrder: "desc" },
+                },
+              }}
+              measures={[
+                {
+                  iri: "value",
+                  label: "value",
+                  __typename: "Measure",
+                },
+              ]}
+            >
+              <ChartContainer>
+                <ChartSvg>
+                  <Bars />
+                </ChartSvg>
+              </ChartContainer>
+            </BarChart>
+          </Box>
+
+          {/* LINE CHART */}
+          <Box sx={{ bg: "monochrome100", p: 5, m: 4 }}>
+            <Text as="h2" variant="heading2" sx={{ pt: 1 }}>
+              <Trans id="detail.card.price.components">Preiskomponenten</Trans>
+            </Text>
+            <LineChart
+              data={observations.map((obs) => ({
+                priceComponent: "Total (exkl. MwSt.)",
+                ...obs,
+              }))}
+              fields={{
+                x: {
+                  componentIri: "period",
+                },
+                y: {
+                  componentIri: "value",
+                },
+              }}
+              measures={[
+                {
+                  iri: "value",
+                  label: "value",
+                  __typename: "Measure",
+                },
+              ]}
+              dimensions={[
+                {
+                  iri: "period",
+                  label: "period",
+                  __typename: "TemporalDimension",
+                },
+              ]}
+              aspectRatio={0.4}
+            >
+              <ChartContainer>
+                <ChartSvg>
+                  <AxisHeightLinear /> <AxisTime /> <AxisTimeDomain />
+                  <Lines />
+                  <InteractionHorizontal />
+                </ChartSvg>
+
+                <Ruler />
+
+                <HoverDotMultiple />
+
+                <Tooltip type={"multiple"} />
+              </ChartContainer>
+              <LegendColor symbol="line" />
+            </LineChart>
           </Box>
         </Box>
       </Flex>
