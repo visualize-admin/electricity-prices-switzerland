0c61ee7ce3f6ab25078704ebd7d6d63e27e524e7
Luc <luc.guillemot@gmail.com>
2020-07-21 16:12:18 +0200

    fix: prevent undefined queryState

--- src/lib/use-query-state.ts
+++ src/lib/use-query-state.ts
@@ -1,6 +1,7 @@
 import { useCallback } from "react";
 import { useRouter } from "next/router";
 import { createDynamicRouteProps } from "../components/links";
+
 const ensureArray = (input: string | string[]): string[] =>
   Array.isArray(input) ? input : [input];
 const ensureString = (input: string | string[]): string =>
@@ -16,15 +17,25 @@ const queryStateKeys = [
   "product",
 ] as const;
 
+const queryStateDefaults = {
+  period: "2020",
+  category: "C1",
+  priceComponent: "total",
+  product: "standard",
+  provider: undefined,
+  municipality: undefined,
+  canton: undefined,
+} as const;
+
 type QueryState = {
   id?: string;
-  period?: string[];
   provider?: string[];
   municipality?: string[];
   canton?: string[];
-  category?: string[];
-  priceComponent?: string[];
-  product?: string[];
+  period: string[];
+  category: string[];
+  priceComponent: string[];
+  product: string[];
 };
 
 // e.g. /de/municipality/4096?municipality=261&period=2020&period=2019
@@ -35,7 +46,7 @@ export const useQueryState = () => {
   const { query, replace, pathname } = useRouter();
 
   const setState = useCallback(
-    (newQueryState: QueryState) => {
+    (newQueryState: Partial<QueryState>) => {
       let newQuery: { [k: string]: string[] } = {};
 
       for (let k of queryStateKeys) {
@@ -55,33 +66,35 @@ export const useQueryState = () => {
     [replace, pathname, query]
   );
 
-  let state: QueryState = {
+  let state: Partial<QueryState> = {
     id: ensureString(query.id),
-  }; // <- somehow we get this from router.query
-  // -> Use the first param
+  };
 
   for (let k of queryStateKeys) {
-    state[k] = ensureArray(query[k]);
+    const v = query[k] ?? queryStateDefaults[k];
+    if (v !== undefined) {
+      state[k] = ensureArray(v);
+    }
   }
 
-  return [state, setState] as const;
+  return [state as QueryState, setState] as const;
 };
 
 type QueryStateSingle = {
   provider?: string;
-  period?: string;
   municipality?: string;
   canton?: string;
-  category?: string;
-  priceComponent?: string;
-  product?: string;
+  period: string;
+  category: string;
+  priceComponent: string;
+  product: string;
 };
 
 export const useQueryStateSingle = () => {
   const { query, replace, pathname } = useRouter();
 
   const setState = useCallback(
-    (newQueryState: QueryStateSingle) => {
+    (newQueryState: Partial<QueryStateSingle>) => {
       let newQuery: { [k: string]: string } = {};
 
       for (let k of queryStateKeys) {
@@ -101,12 +114,14 @@ export const useQueryStateSingle = () => {
     [replace, pathname, query]
   );
 
-  let state: QueryStateSingle = {}; // <- somehow we get this from router.query
-  // -> Use the first param
+  let state: Partial<QueryStateSingle> = {};
 
   for (let k of queryStateKeys) {
-    state[k] = ensureString(query[k]);
+    const v = query[k] ?? queryStateDefaults[k];
+    if (v !== undefined) {
+      state[k] = ensureString(v);
+    }
   }
 
-  return [state, setState] as const;
+  return [state as QueryStateSingle, setState] as const;
 };
