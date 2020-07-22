576eea9ef3363dd2ca84134f1b6d458f101aa1a4
Luc <luc.guillemot@gmail.com>
2020-07-22 21:40:35 +0200

    fix: add mock municipality list

--- src/domain/data.ts
+++ src/domain/data.ts
@@ -75,6 +75,7 @@ export const useColorScale = ({
   }, [observations, accessor, palettes.diverging]);
 };
 
+export const municipalities = ["261", "5590", "5398", "1062", "4221", "3932"];
 export const periods = [
   "2020",
   "2019",
