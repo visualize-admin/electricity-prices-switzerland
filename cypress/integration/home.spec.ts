c1470f819852219fa758be258dfb7ff8f06ecc6d
Jeremy Stucki <jeremy@interactivethings.com>
2020-07-22 10:10:42 +0200

    feat: Add e2e scripts

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,14 +1,12 @@
 describe("The Home Page", () => {
   it("should redirect to /de", () => {
-    cy.request({ url: "http://localhost:3000", followRedirect: false }).should(
-      (response) => {
-        expect(response.status).to.equal(302);
-        expect(response.headers.location).to.equal("/de");
-      }
-    );
+    cy.request({ url: "/", followRedirect: false }).should((response) => {
+      expect(response.status).to.equal(302);
+      expect(response.headers.location).to.equal("/de");
+    });
   });
 
   it("successfully loads", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("/");
   });
 });
