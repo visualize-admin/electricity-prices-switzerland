253fe076bfa26f2ca531bffb3f190ff1b7fa2d1c
Luc <luc.guillemot@gmail.com>
2020-07-23 16:14:38 +0200

    fix: uncomment other charts

--- src/pages/[locale]/municipality/[id].tsx
+++ src/pages/[locale]/municipality/[id].tsx
@@ -54,9 +54,9 @@ const MunicipalityPage = ({ id }: Props) => {
             <Box sx={{ flex: `2 2 ${2 / 3}%` }}>
               <PriceComponentsBarChart id={id} entity="municipality" />
               {/* <PriceComponentsBarChart id={"10219012345"} entity="provider" /> */}
-              {/* <PriceEvolution id={id} entity="municipality" />
+              <PriceEvolution id={id} entity="municipality" />
               <PriceDistributionHistograms id={id} entity="municipality" />
-              <CantonsComparisonRangePlots id={id} entity="municipality" /> */}
+              <CantonsComparisonRangePlots id={id} entity="municipality" />
             </Box>
             <Box sx={{ flex: `1 1 ${1 / 3}%` }}>
               <SelectorMulti />
