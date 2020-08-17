97601c429f4ef786d47e5bd8ab8e34fcf1e9a0e0
Luc <luc.guillemot@gmail.com>
2020-08-17 10:18:10 +0200

    fix: cleanup

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -41,11 +41,10 @@ export const Search = ({ showLabel = true }: { showLabel?: boolean }) => {
       (d) => d.id
     );
   }, [items]);
-  console.log("items", items);
-  // const items = ["one", "two", "three"];
+
   return (
     <>
-      <SF
+      <SearchField
         items={items.map(({ id }) => id)}
         getItemLabel={(id) => itemById.get(id)?.name ?? `[${id}]`}
         setSearchString={setSearchString}
@@ -58,22 +57,9 @@ export const Search = ({ showLabel = true }: { showLabel?: boolean }) => {
         isLoading={gqlQuery.fetching && searchString.length > 0}
         // getItemLabel={(d) => d}
         // items={items}
+
+        // lazy
       />
-      {/* <SearchField
-      id="global-search"
-      label={
-        <Trans id="search.global">
-          Siehe die detaillierte Preisanalyse von Kantone, Gemeinde,
-          Netzbetreiber.
-        </Trans>
-      }
-      items={items.map(({ id }) => id)}
-      getItemLabel={(id) => itemById.get(id)?.name ?? `[${id}]`}
-      onInputValueChange={setInputValue}
-      isLoading={gqlQuery.fetching && inputValue.length > 0}
-      showLabel={showLabel}
-      // lazy
-    />*/}
     </>
   );
 };
@@ -89,7 +75,7 @@ export type SearchFieldProps = {
   showLabel: boolean;
 };
 
-export const SF = ({
+export const SearchField = ({
   label,
   items,
   setSearchString,
@@ -305,242 +291,6 @@ export const SF = ({
           </>
         )}
       </Flex>
-    </Box>
-  );
-};
-
-export const SearchField = ({
-  label,
-  id,
-  items,
-  getItemLabel,
-  onInputValueChange,
-  isLoading,
-  showLabel,
-}: // lazy,
-SearchFieldProps) => {
-  const { query } = useRouter();
-
-  const [inputValue, setInputValue] = useState("");
-
-  const inputItems = items;
-
-  const {
-    isOpen,
-    getToggleButtonProps,
-    getLabelProps,
-    getMenuProps,
-    getInputProps,
-    getComboboxProps,
-    highlightedIndex,
-    getItemProps,
-    openMenu,
-  } = useCombobox({
-    id: `combobox-${id}`,
-    inputValue,
-    items,
-    onStateChange: (changes: $FixMe) => {
-      switch (changes.type) {
-        case useCombobox.stateChangeTypes.InputChange:
-          setInputValue(changes.inputValue);
-          onInputValueChange(inputValue);
-          break;
-        case useCombobox.stateChangeTypes.InputKeyDownEnter:
-        case useCombobox.stateChangeTypes.ItemClick:
-          if (changes.selectedItem) {
-            setInputValue(getItemLabel(changes.inputValue));
-            // setSelectedItem(changes.inputValue);
-          }
-          break;
-        // case useCombobox.stateChangeTypes.InputBlur:
-        // setInputValue("");
-        // onInputValueChange("");
-        // break;
-        default:
-          break;
-      }
-    },
-  });
-
-  return (
-    <Box sx={{ position: "relative", width: "100%", maxWidth: "44rem" }}>
-      <TUILabel
-        {...getLabelProps()}
-        sx={{
-          visibility: showLabel ? "visible" : "hidden",
-          height: showLabel ? "2rem" : 0,
-        }}
-      >
-        <Text
-          variant="paragraph2"
-          sx={{
-            width: "100%",
-            textAlign: "center",
-            color: "monochrome800",
-            mt: 2,
-            mb: 2,
-          }}
-        >
-          {label}
-        </Text>
-      </TUILabel>
-      <Flex
-        sx={{
-          display: "block",
-          py: 0,
-          pl: 2,
-          pr: 6,
-          height: 48,
-
-          appearance: "none",
-          fontSize: "inherit",
-          lineHeight: "inherit",
-          border: "1px solid",
-          borderRadius: 4,
-          color: "text",
-          borderColor: isOpen ? "primary" : "monochrome500",
-          bg: "monochrome100",
-          flexWrap: "wrap",
-          ":focus-within": { borderColor: "primary" },
-          position: "relative",
-        }}
-      >
-        <Box
-          {...getComboboxProps()}
-          sx={{ flexGrow: 1, minWidth: 80, alignSelf: "center", my: 2 }}
-        >
-          <Input
-            {...getInputProps({
-              onFocus: (e) => {
-                e.currentTarget.select();
-              },
-              onClick: () => {
-                openMenu();
-              },
-            })}
-            sx={{
-              display: "block",
-              appearance: "none",
-              fontSize: "inherit",
-              lineHeight: "inherit",
-              border: "none",
-              color: "text",
-              bg: "transparent",
-              borderRadius: 0,
-              p: 0,
-              ":focus": { outline: 0 },
-            }}
-          />
-        </Box>
-        <Button
-          {...getToggleButtonProps()}
-          aria-label={"toggle menu"}
-          variant="reset"
-          sx={{
-            color: "monochrome800",
-            p: 0,
-            mr: 1,
-            position: "absolute",
-            right: 0,
-            top: "50%",
-            height: 48,
-            transform: "translateY(-50%)",
-          }}
-        >
-          {isOpen ? <Icon name="chevronup" /> : <Icon name="chevrondown" />}
-        </Button>
-      </Flex>
-
-      <Box
-        as="ul"
-        sx={{
-          listStyle: "none",
-          borderRadius: "default",
-          boxShadow: "tooltip",
-          bg: "monochrome100",
-          mt: 1,
-          p: 0,
-          position: "absolute",
-          width: "100%",
-          zIndex: 999,
-        }}
-        style={{ display: isOpen ? "block" : "none" }}
-        {...getMenuProps()}
-      >
-        {isOpen && isLoading ? (
-          <Text
-            as="li"
-            variant="paragraph2"
-            sx={{
-              color: "secondary",
-              p: 3,
-              m: 0,
-            }}
-          >
-            <Trans id="combobox.isloading">Resultate laden …</Trans>
-          </Text>
-        ) : isOpen && !isLoading && inputItems.length === 0 ? (
-          <Text
-            as="li"
-            variant="paragraph2"
-            sx={{
-              color: "secondary",
-              p: 3,
-              m: 0,
-            }}
-          >
-            {inputValue === "" ? (
-              // && lazy
-              <Trans id="combobox.prompt">Bezeichnung eingeben …</Trans>
-            ) : (
-              <Trans id="combobox.noitems">Keine Einträge</Trans>
-            )}
-          </Text>
-        ) : (
-          <>
-            {inputItems.map((item, index) => (
-              // <LocalizedLink
-              //   key={`${item}${index}`}
-              //   pathname="/[locale]/provider/[id]"
-              //   query={{ ...query, id: item }}
-              //   passHref
-              // >
-              <Flex
-                {...getItemProps({ item, index })}
-                key={`${item}${index}`}
-                as="li"
-                sx={{
-                  mx: 4,
-                  py: 1,
-                  px: 4,
-                  alignItems: "center",
-                  height: "3.5rem",
-                  lineHeight: 1,
-                  color: "text",
-                  textDecoration: "none",
-                  ":hover": {
-                    cursor: "pointer",
-                    bg: "mutedDarker",
-                  },
-                  ":focus": {
-                    outline: 0,
-                    bg: "primaryLight",
-                  },
-                }}
-              >
-                <Text variant="paragraph1" sx={{ flexGrow: 1 }}>
-                  {getItemLabel(item)}
-                </Text>
-
-                <Box sx={{ width: "24px", flexShrink: 0 }}>
-                  <Icon name="chevronright"></Icon>
-                </Box>
-              </Flex>
-              // </LocalizedLink>
-            ))}
-          </>
-        )}
-      </Box>
     </Box>
   );
 };
