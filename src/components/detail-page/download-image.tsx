d61eb962668173daea53b146d5283a4ddea5a7ee
Luc <luc.guillemot@gmail.com>
2020-08-18 17:08:30 +0200

    feat: add query param to download url

--- src/components/detail-page/download-image.tsx
+++ src/components/detail-page/download-image.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { Trans } from "@lingui/macro";
 import { useLocale } from "../../lib/use-locale";
 import { Entity } from "../../domain/data";
+import { useQueryState } from "../../lib/use-query-state";
+import { useRouter } from "next/router";
 import { Link as TUILink } from "@theme-ui/components";
 
 export type DownloadChart =
@@ -15,20 +17,38 @@ interface Props {
   id?: string;
   elementId: string;
   fileName: string;
+  chart: DownloadChart;
 }
 
-export const DownloadImage = ({ elementId, fileName, entity, id }: Props) => {
+export const DownloadImage = ({
+  elementId,
+  fileName,
+  entity,
+  id,
+  chart,
+}: Props) => {
   const locale = useLocale();
+  const [{ period, category, product }] = useQueryState();
+  const { query, replace, pathname } = useRouter();
+
   const [origin, setOrigin] = React.useState<undefined | string>(undefined);
+
   React.useEffect(() => {
     setOrigin(window.location.origin);
   }, [setOrigin]);
 
+  const params = `${period.reduce(
+    (acc, cur, i) => acc.concat(`${i > 0 ? "&" : ""}period=${cur}`),
+    ""
+  )}`;
+  const url = `${origin}/${locale}/${entity}/${id}?chart=${chart}`;
+
   // FIXME: use encodeURIComponent
-  const url =
-    entity && id
-      ? `${origin}/api/screenshot?url=${origin}/${locale}/${entity}/${id}&element=${elementId}&download=${fileName}-image`
-      : `${origin}/api/screenshot?url=${origin}/${locale}&element=${elementId}&download=${fileName}-image`;
+  const downLoadUrl =
+    entity && id && chart
+      ? `${origin}/api/screenshot?url=${url}&element=${elementId}&download=${fileName}-image&deviceScaleFactor=1.5`
+      : // Map
+        `${origin}/api/screenshot?url=${origin}/${locale}&element=${elementId}&download=${fileName}-image&deviceScaleFactor=1.5`;
 
   return (
     <TUILink
