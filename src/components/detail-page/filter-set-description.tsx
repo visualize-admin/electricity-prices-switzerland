24882d58712b9f86197ba23b3c0e7ac401897037
Jeremy Stucki <jeremy@interactivethings.com>
2020-09-16 11:20:54 +0200

    fix: Correct translation in filterset description

--- src/components/detail-page/filter-set-description.tsx
+++ src/components/detail-page/filter-set-description.tsx
@@ -1,17 +1,17 @@
 import { Trans } from "@lingui/macro";
-import { Text } from "@theme-ui/components";
+import { Text, Box } from "@theme-ui/components";
 import * as React from "react";
 import { getLocalizedLabel } from "../../domain/translation";
 import { useI18n } from "../i18n-context";
 
 type FilterSet = {
-  operator?: string;
-  municipality?: string;
-  canton?: string;
-  period?: string;
-  category?: string;
-  priceComponent?: string | React.ReactNode;
-  product?: string;
+  operator: string;
+  municipality: string;
+  canton: string;
+  period: string;
+  category: string;
+  priceComponent: string;
+  product: string;
 };
 
 export const FilterSetDescription = ({
@@ -25,13 +25,26 @@ export const FilterSetDescription = ({
       variant="paragraph1"
       sx={{ my: 4, color: "monochrome800", fontWeight: "light" }}
     >
-      {Object.keys(filters).map((filterKey, i) => (
-        <React.Fragment key={filterKey}>
-          <span>{getLocalizedLabel({ i18n, id: filterKey })}</span> (
-          <span>{filters[filterKey as keyof FilterSet]}</span>)
-          {i < Object.keys(filters).length - 1 && ", "}
-        </React.Fragment>
-      ))}
+      {Object.entries(filters).map(([key, value], i) => {
+        console.log(key);
+        if (!value) {
+          return null;
+        }
+
+        return (
+          <React.Fragment key={key}>
+            <Box as="span">{getLocalizedLabel({ i18n, id: key })}</Box>
+            {": "}
+            <Box as="span" sx={{ fontWeight: "bold" }}>
+              {getLocalizedLabel({
+                i18n,
+                id: value,
+              })}
+            </Box>
+            {i < Object.keys(filters).length - 1 && ", "}
+          </React.Fragment>
+        );
+      })}
     </Text>
   );
 };
