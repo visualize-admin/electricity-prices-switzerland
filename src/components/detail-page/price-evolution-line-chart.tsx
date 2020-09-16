86bb903eb4d0f3ca6ef646743c55c70b6a489cac
Luc <luc.guillemot@gmail.com>
2020-09-16 11:46:49 +0200

    fix: add new component specific for line chart

--- src/components/detail-page/price-evolution-line-chart.tsx
+++ src/components/detail-page/price-evolution-line-chart.tsx
@@ -76,18 +76,6 @@ export const PriceEvolution = ({
     ? EMPTY_ARRAY
     : observationsQuery.data?.observations ?? EMPTY_ARRAY;
 
-  // Add a unique ID for the combinations municipality+operator
-  const withUniqueEntityId = observations.map((obs) => ({
-    uniqueId:
-      obs.__typename === "OperatorObservation"
-        ? `${obs.municipalityLabel}, ${obs.operatorLabel}`
-        : obs.cantonLabel,
-    ...obs,
-  }));
-
-  const hasMultipleLines =
-    new Set(withUniqueEntityId.map((obs) => obs.uniqueId)).size > 1;
-
   return (
     <Card
       title={
@@ -109,95 +97,100 @@ export const PriceEvolution = ({
         <NoDataHint />
       ) : (
         <div className={DOWNLOAD_ID}>
-          {priceComponents.map((pc, i) => (
-            <PriceEvolutionLineChart
-              key={pc}
-              hasMultipleLines={hasMultipleLines}
-              observations={withUniqueEntityId as GenericObservation[]}
+          <WithClassName downloadId={DOWNLOAD_ID}>
+            <PriceEvolutionLineCharts
+              observations={observations as GenericObservation[]}
               entity={entity}
-              priceComponent={pc as PriceComponent}
-              withLegend={i === 0 && hasMultipleLines}
             />
-          ))}
+          </WithClassName>
         </div>
       )}
     </Card>
   );
 };
 
-const PriceEvolutionLineChart = memo(
+const PriceEvolutionLineCharts = memo(
   ({
-    hasMultipleLines,
     observations,
     entity,
-    priceComponent,
-    withLegend,
   }: {
-    hasMultipleLines: boolean;
     observations: GenericObservation[];
     entity: Entity;
-    priceComponent: PriceComponent;
-    withLegend: boolean;
   }) => {
     const i18n = useI18n();
 
+    // Add a unique ID for the combinations municipality+operator
+    const withUniqueEntityId = observations.map((obs) => ({
+      uniqueId:
+        obs.__typename === "OperatorObservation"
+          ? `${obs.municipalityLabel}, ${obs.operatorLabel}`
+          : obs.cantonLabel,
+      ...obs,
+    }));
+
+    const hasMultipleLines =
+      new Set(withUniqueEntityId.map((obs) => obs.uniqueId)).size > 1;
+
     return (
-      <WithClassName downloadId={DOWNLOAD_ID}>
-        <Box sx={{ my: 4 }}>
-          <LineChart
-            data={observations}
-            fields={{
-              x: {
-                componentIri: "period",
-              },
-              y: {
-                componentIri: priceComponent,
-              },
-              segment: hasMultipleLines
-                ? {
-                    componentIri: "uniqueId",
+      <>
+        {priceComponents.map((pc, i) => {
+          return (
+            <Box sx={{ my: 4 }}>
+              <LineChart
+                data={withUniqueEntityId}
+                fields={{
+                  x: {
+                    componentIri: "period",
+                  },
+                  y: {
+                    componentIri: pc,
+                  },
+                  segment: hasMultipleLines
+                    ? {
+                        componentIri: "uniqueId",
+                        palette: "elcom",
+                      }
+                    : undefined,
+                }}
+                measures={[
+                  {
+                    iri: pc,
+                    label: getLocalizedLabel({ i18n, id: pc }),
+                    __typename: "Measure",
+                  },
+                ]}
+                dimensions={[
+                  {
+                    iri: "period",
+                    label: "period",
+                    __typename: "TemporalDimension",
+                  },
+                ]}
+                aspectRatio={0.2}
+              >
+                {i === 0 && hasMultipleLines && (
+                  <Box sx={{ mb: 6 }}>
+                    <LegendColor symbol="line" />
+                  </Box>
+                )}
+                <ChartContainer>
+                  <ChartSvg>
+                    <AxisHeightLinear /> <AxisTime />
+                    <Lines />
+                    <InteractionHorizontal />
+                  </ChartSvg>
 
-                    palette: "elcom",
-                  }
-                : undefined,
-            }}
-            measures={[
-              {
-                iri: priceComponent,
-                label: getLocalizedLabel({ i18n, id: priceComponent }),
-                __typename: "Measure",
-              },
-            ]}
-            dimensions={[
-              {
-                iri: "period",
-                label: "period",
-                __typename: "TemporalDimension",
-              },
-            ]}
-            aspectRatio={0.2}
-          >
-            {withLegend && (
-              <Box sx={{ mb: 6 }}>
-                <LegendColor symbol="line" />
-              </Box>
-            )}
-            <ChartContainer>
-              <ChartSvg>
-                <AxisHeightLinear /> <AxisTime />
-                <Lines />
-                <InteractionHorizontal />
-              </ChartSvg>
+                  {hasMultipleLines && <Ruler />}
 
-              {hasMultipleLines && <Ruler />}
+                  <HoverDotMultiple />
 
-              <HoverDotMultiple />
-
-              <Tooltip type={hasMultipleLines ? "multiple" : "single"} />
-            </ChartContainer>
-          </LineChart>
-        </Box>
-      </WithClassName>
+                  <Tooltip type={hasMultipleLines ? "multiple" : "single"} />
+                </ChartContainer>
+              </LineChart>
+            </Box>
+          );
+        })}
+      </>
     );
   }
 );
