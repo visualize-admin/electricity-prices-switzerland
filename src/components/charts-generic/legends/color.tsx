ae9a6cfaac0ce16dbd731b60045453940a4215a1
Luc <luc.guillemot@gmail.com>
2020-09-17 16:45:27 +0200

    feat: make line legend slightly bigger

--- src/components/charts-generic/legends/color.tsx
+++ src/components/charts-generic/legends/color.tsx
@@ -55,7 +55,7 @@ export const LegendItem = ({
         display: "block",
         left: -2,
         width: ".5rem",
-        height: symbol === "square" || symbol === "circle" ? `.5rem` : 2,
+        height: symbol === "square" || symbol === "circle" ? `.5rem` : 3,
         borderRadius: symbol === "circle" ? "50%" : 0,
         bg: color,
       },
