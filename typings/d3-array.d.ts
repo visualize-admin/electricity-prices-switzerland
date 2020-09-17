33853a867da59287679561e2052ec0c41920bc53
Luc <luc.guillemot@gmail.com>
2020-09-17 10:13:16 +0200

    fix: add “entity” missing nesting level

--- typings/d3-array.d.ts
+++ typings/d3-array.d.ts
@@ -4,6 +4,7 @@ declare module "d3-array" {
   export function groups<TObject, TKey>(
     a: Iterable<TObject>,
     key: (value: TObject) => TKey,
+    key?: (value: TObject) => TKey,
     key?: (value: TObject) => TKey
   ): Array<TKey, TObject[]>;
 }
