189ae0b20d9d3c14f60e92864832f2b865350397
Luc <luc.guillemot@gmail.com>
2020-09-17 16:44:57 +0200

    fix: d3-array type definition

--- typings/d3-array.d.ts
+++ typings/d3-array.d.ts
@@ -3,8 +3,8 @@ import * as d3Array from "d3-array";
 declare module "d3-array" {
   export function groups<TObject, TKey>(
     a: Iterable<TObject>,
-    key: (value: TObject) => TKey,
-    key?: (value: TObject) => TKey,
-    key?: (value: TObject) => TKey
+    key: (value: TObject) => TKey | number,
+    key?: (value: TObject) => TKey | number,
+    key?: (value: TObject) => TKey | number
   ): Array<TKey, TObject[]>;
 }
