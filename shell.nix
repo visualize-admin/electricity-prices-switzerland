c4fc7d8f4186b512ecbfbaba33018e7e032e38fd
Jeremy Stucki <jeremy@interactivethings.com>
2021-01-28 09:47:23 +0100

    chore: Upgrade yarn, fix  node version in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 let
   pkgs = import <nixpkgs> { };
   nodejs = pkgs.nodejs-12_x;
+  yarn = pkgs.yarn.override { inherit nodejs; };
 
 in pkgs.mkShell {
   buildInputs = [
     pkgs.darwin.apple_sdk.frameworks.CoreServices
-    pkgs.yarn
+    yarn
     nodejs
   ];
 }
