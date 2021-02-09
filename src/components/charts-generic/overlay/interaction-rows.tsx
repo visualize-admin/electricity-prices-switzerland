c4d9969af4328cbc7ad9ce447af8c0d7f933e856
Luc <luc.guillemot@gmail.com>
2021-02-09 10:43:39 +0100

    feat: add debug mode to InteractionRows

--- src/components/charts-generic/overlay/interaction-rows.tsx
+++ src/components/charts-generic/overlay/interaction-rows.tsx
@@ -5,7 +5,7 @@ import { useInteraction } from "../use-interaction";
 import { DOT_RADIUS, RangePlotState } from "../rangeplot/rangeplot-state";
 import { isNumber } from "../../../domain/helpers";
 
-export const InteractionRows = () => {
+export const InteractionRows = ({ debug = false }: { debug: boolean }) => {
   const [, dispatch] = useInteraction();
 
   const {
@@ -14,7 +14,7 @@ export const InteractionRows = () => {
     getX,
     yScale,
   } = useChartState() as RangePlotState;
-  const { margins, chartWidth } = bounds;
+  const { margins, chartWidth, chartHeight } = bounds;
 
   const showTooltip = (d: GenericObservation) => {
     dispatch({
@@ -27,25 +27,68 @@ export const InteractionRows = () => {
       type: "INTERACTION_HIDE",
     });
   };
+  console.log(margins);
   return (
     <>
-      <g transform={`translate(${margins.left}, ${margins.top})`}>
+      <g
+        transform={`translate(0, ${(margins.annotations ?? 0) + margins.top})`}
+      >
         {rangeGroups.map((row, i) => (
           <rect
             key={row[0]}
             x={0}
-            y={
-              (yScale(row[0]) || yScale.bandwidth() * i + margins.top) -
-              yScale.bandwidth() / 2
-            }
-            width={chartWidth}
+            y={yScale(row[0])}
+            width={margins.left + chartWidth + margins.right}
             height={yScale.bandwidth()}
-            fillOpacity={0}
+            fillOpacity={debug ? 0.2 : 0}
+            fill="hotpink"
+            stroke={debug ? "hotpink" : "none"}
             onMouseOut={hideTooltip}
             onMouseOver={() => showTooltip(row[1][0])}
           />
         ))}
       </g>
+      {debug && (
+        <>
+          <g transform={`translate(0, 0)`}>
+            <rect
+              x={0}
+              y={0}
+              width={margins.left + chartWidth + margins.right}
+              height={margins.annotations}
+              fillOpacity={0.2}
+              fill="Orchid"
+              stroke="Orchid"
+            />
+          </g>
+          <g transform={`translate(0, ${margins.annotations})`}>
+            <rect
+              x={0}
+              y={0}
+              width={margins.left + chartWidth + margins.right}
+              height={margins.top}
+              fillOpacity={0.2}
+              fill="LightSeaGreen"
+              stroke="LightSeaGreen"
+            />
+          </g>
+          <g
+            transform={`translate(0, ${
+              (margins.annotations ?? 0) + margins.top + chartHeight
+            })`}
+          >
+            <rect
+              x={0}
+              y={0}
+              width={margins.left + chartWidth + margins.right}
+              height={margins.bottom}
+              fillOpacity={0.2}
+              fill="green"
+              stroke="green"
+            />
+          </g>
+        </>
+      )}
     </>
   );
 };
