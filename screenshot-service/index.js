246a3e8553e90fb19352b89f0925e67f05c5ffbc
Jeremy Stucki <jeremy@interactivethings.com>
2021-03-10 12:02:01 +0100

    fix: Add dynamic port to screenshot-service

--- screenshot-service/index.js
+++ screenshot-service/index.js
@@ -1,9 +1,11 @@
 const polka = require("polka");
 const { handleScreenshot } = require("./dist/screenshot");
 
+const PORT = process.env.PORT || 3001;
+
 polka()
   .get("/api/screenshot", handleScreenshot)
-  .listen(3001, (err) => {
+  .listen(PORT, (err) => {
     if (err) throw err;
-    console.log(`> Running on http://localhost:3001`);
+    console.log(`> Running screenshot-service on http://localhost:${PORT}`);
   });
