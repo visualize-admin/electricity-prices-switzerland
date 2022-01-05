7d77caba246f3edaa2e6e1512afe47d532de6558
Patrick Browne <pt.browne@gmail.com>
2022-01-05 15:29:49 +0100

    fix: Increase specificity of the stack children slightly

--- src/components/stack.tsx
+++ src/components/stack.tsx
@@ -14,7 +14,8 @@ const Stack = ({
       sx={{
         display: "flex",
         flexDirection: direction,
-        "& > * + *": {
+        "& > * + *:not(_)": {
+          // The not(_) is to increase specificity slightly
           [direction === "column" ? "mt" : "ml"]: spacing,
         },
       }}
