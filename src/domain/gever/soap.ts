247cb9c60217561b8d571609294f3188cadddfee
Patrick Browne <pt.browne@gmail.com>
2022-04-26 18:26:42 +0200

    feat: Log EIAM certificate content

--- src/domain/gever/soap.ts
+++ src/domain/gever/soap.ts
@@ -21,6 +21,8 @@ const getCertificateContent = () => {
 
 export const makeSslConfiguredAgent = () => {
   const pfx = getCertificateContent();
+  console.log("pfx", pfx);
+  console.log("cert", process.env.EIAM_CERTIFICATE_CONTENT);
   const CERTIFICATE_PASSWORD = process.env.EIAM_CERTIFICATE_PASSWORD;
 
   if (!CERTIFICATE_PASSWORD) {
