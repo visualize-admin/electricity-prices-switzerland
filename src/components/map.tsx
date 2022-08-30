015306c87c06a0c691d4135a5185699cc855899c
Patrick Browne <pt.browne@gmail.com>
2022-08-30 11:20:01 +0200

    feat: Use data for previous year if current year not available

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -222,6 +222,24 @@ export const HighlightContext = createContext({
   >,
 });
 
+const fallback = async <T extends unknown>(arr: (() => Promise<T>)[]) => {
+  let errors: unknown[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    try {
+      return await arr[i]();
+    } catch (e) {
+      errors.push(e);
+      if (i === arr.length - 1) {
+        throw new Error(
+          `Could not fallback, errors: ${errors.map((e) => {
+            return e instanceof Error ? e.message : e;
+          })}`
+        );
+      }
+    }
+  }
+};
+
 export const ChoroplethMap = ({
   year,
   observations,
@@ -269,9 +287,10 @@ export const ChoroplethMap = ({
   useEffect(() => {
     const load = async () => {
       try {
-        const topo = await import(
-          `swiss-maps/${parseInt(year, 10) - 1}/ch-combined.json`
-        );
+        const topo = await fallback([
+          () => import(`swiss-maps/${parseInt(year, 10) - 1}/ch-combined.json`),
+          () => import(`swiss-maps/${parseInt(year, 10) - 2}/ch-combined.json`),
+        ]);
 
         const municipalities = topojsonFeature(
           topo,
