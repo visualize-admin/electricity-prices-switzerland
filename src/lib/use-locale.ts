c88da555f47e5128326ae990cc2cd5ce9607f441
Patrick Browne <pt.browne@gmail.com>
2023-02-20 17:25:31 +0100

    fix: Last type fix on locale hopefully

--- src/lib/use-locale.ts
+++ src/lib/use-locale.ts
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
-import { defaultLocale } from "../locales/locales";
+import { defaultLocale, Locale } from "../locales/locales";
 
 export const useLocale = () => {
   const { i18n } = useTranslation();
-  return i18n.language || defaultLocale;
+  return (i18n.language || defaultLocale) as Locale;
 };
