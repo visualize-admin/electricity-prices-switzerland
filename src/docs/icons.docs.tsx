c2495f8d19edf0356b78c4a58937b68d889ecc4b
Patrick Browne <pt.browne@gmail.com>
2023-05-22 17:29:28 +0200

    feat: Add page listing all icons

---
+++ src/docs/icons.docs.tsx
@@ -0,0 +1,235 @@
+import { Button, Box } from "@theme-ui/components";
+import { markdown, ReactSpecimen } from "catalog";
+import { FilterSetDescription } from "../components/detail-page/filter-set-description";
+
+import { IconAdd } from "../icons/ic-add";
+import { IconAreaChart } from "../icons/ic-area-chart";
+import { IconArrowDown } from "../icons/ic-arrow-down";
+import { IconArrowRight } from "../icons/ic-arrow-right";
+import { IconBarChart } from "../icons/ic-bar-chart";
+import { IconCaretDown } from "../icons/ic-caret-down";
+import { IconCaretRight } from "../icons/ic-caret-right";
+import { IconCheck } from "../icons/ic-check";
+import { IconChevronDown } from "../icons/ic-chevron-down";
+import { IconChevronLeft } from "../icons/ic-chevron-left";
+import { IconChevronRight } from "../icons/ic-chevron-right";
+import { IconChevronUp } from "../icons/ic-chevron-up";
+import { IconClear } from "../icons/ic-clear";
+import { IconColumnChart } from "../icons/ic-column-chart";
+import { IconCopy } from "../icons/ic-copy";
+import { IconDatasetPublished } from "../icons/ic-dataset-published";
+import { IconDatasetWarning } from "../icons/ic-dataset-warning";
+import { IconDataset } from "../icons/ic-dataset";
+import { IconEmbed } from "../icons/ic-embed";
+import { IconExcel } from "../icons/ic-excel";
+import { IconFacebook } from "../icons/ic-facebook";
+import { IconFilter } from "../icons/ic-filter";
+import { IconHintWarning } from "../icons/ic-hint-warning";
+import { IconImage } from "../icons/ic-image";
+import { IconInfo } from "../icons/ic-info";
+import { IconLineChart } from "../icons/ic-line-chart";
+import { IconLoading } from "../icons/ic-loading";
+import { IconMail } from "../icons/ic-mail";
+import { IconPdf } from "../icons/ic-pdf";
+import { IconPieChart } from "../icons/ic-pie-chart";
+import { IconResize } from "../icons/ic-resize";
+import { IconScatterplot } from "../icons/ic-scatterplot";
+import { IconSearch } from "../icons/ic-search";
+import { IconSegment } from "../icons/ic-segment";
+import { IconShare } from "../icons/ic-share";
+import { IconSort } from "../icons/ic-sort";
+import { IconTable } from "../icons/ic-table";
+import { IconText } from "../icons/ic-text";
+import { IconTwitter } from "../icons/ic-twitter";
+import { IconUnfold } from "../icons/ic-unfold";
+import { IconWarning } from "../icons/ic-warning";
+import { IconX } from "../icons/ic-x";
+import { IconY } from "../icons/ic-y";
+
+export default () => markdown`>
+
+  ${(
+    <ReactSpecimen span={6}>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+          gap: "1rem",
+        }}
+      >
+        <div>
+          <div>IconAdd</div>
+          <IconAdd size={20} />
+        </div>
+        <div>
+          <div>IconAreaChart</div>
+          <IconAreaChart size={20} />
+        </div>
+        <div>
+          <div>IconArrowDown</div>
+          <IconArrowDown size={20} />
+        </div>
+        <div>
+          <div>IconArrowRight</div>
+          <IconArrowRight size={20} />
+        </div>
+        <div>
+          <div>IconBarChart</div>
+          <IconBarChart size={20} />
+        </div>
+        <div>
+          <div>IconCaretDown</div>
+          <IconCaretDown size={20} />
+        </div>
+        <div>
+          <div>IconCaretRight</div>
+          <IconCaretRight size={20} />
+        </div>
+        <div>
+          <div>IconCheck</div>
+          <IconCheck size={20} />
+        </div>
+        <div>
+          <div>IconChevronDown</div>
+          <IconChevronDown size={20} />
+        </div>
+        <div>
+          <div>IconChevronLeft</div>
+          <IconChevronLeft size={20} />
+        </div>
+        <div>
+          <div>IconChevronRight</div>
+          <IconChevronRight size={20} />
+        </div>
+        <div>
+          <div>IconChevronUp</div>
+          <IconChevronUp size={20} />
+        </div>
+        <div>
+          <div>IconClear</div>
+          <IconClear size={20} />
+        </div>
+        <div>
+          <div>IconColumnChart</div>
+          <IconColumnChart size={20} />
+        </div>
+        <div>
+          <div>IconCopy</div>
+          <IconCopy size={20} />
+        </div>
+        <div>
+          <div>IconDatasetPublished</div>
+          <IconDatasetPublished size={20} />
+        </div>
+        <div>
+          <div>IconDatasetWarning</div>
+          <IconDatasetWarning size={20} />
+        </div>
+        <div>
+          <div>IconDataset</div>
+          <IconDataset size={20} />
+        </div>
+        <div>
+          <div>IconEmbed</div>
+          <IconEmbed size={20} />
+        </div>
+        <div>
+          <div>IconExcel</div>
+          <IconExcel size={20} />
+        </div>
+        <div>
+          <div>IconFacebook</div>
+          <IconFacebook size={20} />
+        </div>
+        <div>
+          <div>IconFilter</div>
+          <IconFilter size={20} />
+        </div>
+        <div>
+          <div>IconHintWarning</div>
+          <IconHintWarning size={20} />
+        </div>
+        <div>
+          <div>IconImage</div>
+          <IconImage size={20} />
+        </div>
+        <div>
+          <div>IconInfo</div>
+          <IconInfo size={20} />
+        </div>
+        <div>
+          <div>IconLineChart</div>
+          <IconLineChart size={20} />
+        </div>
+        <div>
+          <div>IconLoading</div>
+          <IconLoading size={20} />
+        </div>
+        <div>
+          <div>IconMail</div>
+          <IconMail size={20} />
+        </div>
+        <div>
+          <div>IconPdf</div>
+          <IconPdf size={20} />
+        </div>
+        <div>
+          <div>IconPieChart</div>
+          <IconPieChart size={20} />
+        </div>
+        <div>
+          <div>IconResize</div>
+          <IconResize size={20} />
+        </div>
+        <div>
+          <div>IconScatterplot</div>
+          <IconScatterplot size={20} />
+        </div>
+        <div>
+          <div>IconSearch</div>
+          <IconSearch size={20} />
+        </div>
+        <div>
+          <div>IconSegment</div>
+          <IconSegment size={20} />
+        </div>
+        <div>
+          <div>IconShare</div>
+          <IconShare size={20} />
+        </div>
+        <div>
+          <div>IconSort</div>
+          <IconSort size={20} />
+        </div>
+        <div>
+          <div>IconTable</div>
+          <IconTable size={20} />
+        </div>
+        <div>
+          <div>IconText</div>
+          <IconText size={20} />
+        </div>
+        <div>
+          <div>IconTwitter</div>
+          <IconTwitter size={20} />
+        </div>
+        <div>
+          <div>IconUnfold</div>
+          <IconUnfold size={20} />
+        </div>
+        <div>
+          <div>IconWarning</div>
+          <IconWarning size={20} />
+        </div>
+        <div>
+          <div>IconX</div>
+          <IconX size={20} />
+        </div>
+        <div>
+          <div>IconY</div>
+          <IconY size={20} />
+        </div>
+      </Box>
+    </ReactSpecimen>
+  )}
+`;
