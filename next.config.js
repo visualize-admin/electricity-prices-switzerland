2b239bebc921a2201bdd3da3392043269c9c49e1
Patrick Browne <pt.browne@gmail.com>
2023-05-24 17:06:25 +0200

    Revert "feat: Add git sha inside app version"
    32192518112588fb5da176987d3367b4844130f1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const pkg = require("./package.json");
-const { getGitCommitSHA } = require("./src/lib/git");
 const withMDX = require("@next/mdx")();
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
@@ -7,63 +6,62 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 const { locales, defaultLocale } = require("./src/locales/locales.json");
 
-module.exports = () => {
-  const buildEnv = {
-    VERSION: `v${pkg.version}-${getGitCommitSHA().slice(0, 7)}`,
-    DEPLOYMENT: process.env.DEPLOYMENT,
-    CURRENT_PERIOD: process.env.CURRENT_PERIOD || "2023",
-    FIRST_PERIOD: process.env.FIRST_PERIOD || "2009",
-  };
-
-  console.log("Build Environment:", buildEnv);
-  console.log("Matomo ID:", process.env.MATOMO_ID);
-
-  let i18nDomains;
-
-  try {
-    if (process.env.I18N_DOMAINS !== undefined) {
-      const domainsEnv = JSON.parse(process.env.I18N_DOMAINS);
-
-      i18nDomains = Object.entries(domainsEnv).map(([locale, domain]) => {
-        return {
-          domain,
-          defaultLocale: locale,
-        };
-      });
-    }
-  } catch (e) {
-    console.error("I18N_DOMAINS parsing failed:", e.message);
-  }
-  return withBundleAnalyzer(
-    withMDX({
-      webpack5: true,
-
-      // Build-time env variables
-      env: buildEnv,
-
-      pageExtensions: ["js", "ts", "tsx", "mdx"],
-
-      i18n: {
-        locales,
-        defaultLocale,
-        domains: i18nDomains,
-        localeDetection: false,
-      },
-
-      webpack(config, { dev, isServer, defaultLoaders }) {
-        config.module.rules.push({
-          test: /\.(graphql|gql)$/,
-          exclude: /node_modules/,
-          loader: "graphql-tag/loader",
-        });
-        config.module.rules.push({
-          test: /\.xml$/,
-          exclude: /node_modules/,
-          loader: "raw-loader",
-        });
-
-        return config;
-      },
-    })
-  );
+const buildEnv = {
+  VERSION: `v${pkg.version}`,
+  DEPLOYMENT: process.env.DEPLOYMENT,
+  CURRENT_PERIOD: process.env.CURRENT_PERIOD || "2023",
+  FIRST_PERIOD: process.env.FIRST_PERIOD || "2009",
 };
+
+console.log("Build Environment:", buildEnv);
+console.log("Matomo ID:", process.env.MATOMO_ID);
+
+let i18nDomains;
+
+try {
+  if (process.env.I18N_DOMAINS !== undefined) {
+    const domainsEnv = JSON.parse(process.env.I18N_DOMAINS);
+
+    i18nDomains = Object.entries(domainsEnv).map(([locale, domain]) => {
+      return {
+        domain,
+        defaultLocale: locale,
+      };
+    });
+  }
+} catch (e) {
+  console.error("I18N_DOMAINS parsing failed:", e.message);
+}
+
+module.exports = withBundleAnalyzer(
+  withMDX({
+    webpack5: true,
+
+    // Build-time env variables
+    env: buildEnv,
+
+    pageExtensions: ["js", "ts", "tsx", "mdx"],
+
+    i18n: {
+      locales,
+      defaultLocale,
+      domains: i18nDomains,
+      localeDetection: false,
+    },
+
+    webpack(config, { dev, isServer, defaultLoaders }) {
+      config.module.rules.push({
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: "graphql-tag/loader",
+      });
+      config.module.rules.push({
+        test: /\.xml$/,
+        exclude: /node_modules/,
+        loader: "raw-loader",
+      });
+
+      return config;
+    },
+  })
+);
