1bed1f38a02c2459b9382d744032fa594cccc352
Patrick Browne <pt.browne@gmail.com>
2023-09-26 16:20:10 +0200

    feat: Make sure the downloaded does not contain any bad character

--- src/pages/api/download-operator-document/[reference].ts
+++ src/pages/api/download-operator-document/[reference].ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { downloadGeverDocument } from "../../../domain/gever";
 
+// Taken from node.js source code
+// @see https://github.com/nodejs/node/blob/main/lib/_http_common.js#L216C1-L216C51
+const badCharRegex = /[^\t\x20-\x7e\x80-\xff]/gi;
+
+const removeBadChars = (str: string) => {
+  return str.replace(badCharRegex, "");
+};
+
+const sanitizeHeaderValue = (str: string) => {
+  return encodeURIComponent(removeBadChars(str));
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const reference = req.query.reference as string;
   const filename = (req.query.filename as string) || "document.pdf";
@@ -9,9 +21,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   console.log(`Downloading ${fileAttrs.name}`);
   res.setHeader(
     "Content-Disposition",
-    `attachment; filename="${encodeURIComponent(fileAttrs.name)}.${
-      fileAttrs.extension
-    }"`
+    `attachment; filename="${sanitizeHeaderValue(
+      `${fileAttrs.name}.${fileAttrs.extension}`
+    )}"`
   );
   res.send(fileAttrs.buffer);
   res.end();
