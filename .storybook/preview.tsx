09301d3c7aa255c3ffb236e0e57bba6966143643
Patrick Browne <pt.browne@gmail.com>
2023-10-03 13:45:24 +0200

    feat: Sort stories

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -15,6 +15,12 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    options: {
+      storySort: (a, b) =>
+        a.id === b.id
+          ? 0
+          : a.id.localeCompare(b.id, undefined, { numeric: true }),
+    },
   },
   decorators: [
     (Story) => (
