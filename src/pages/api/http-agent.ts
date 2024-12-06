768d6b733ca6cbf926a6c713fb168b00720ee1e5
Patrick Browne <pt.browne@gmail.com>
2024-12-06 13:35:11 +0100

    feat: Configure global undici dispatcher

---
+++ src/pages/api/http-agent.ts
@@ -0,0 +1,18 @@
+import { ProxyAgent, setGlobalDispatcher } from "undici";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __undiciProxyAgent__: ProxyAgent | undefined;
+}
+
+export const setupUndiciHttpAgent = () => {
+  const HTTP_PROXY = process.env.HTTP_PROXY;
+  if (!HTTP_PROXY) {
+    return;
+  }
+  if (!globalThis.__undiciProxyAgent__) {
+    console.log("Setting up undici proxy agent: ", HTTP_PROXY);
+    globalThis.__undiciProxyAgent__ = new ProxyAgent(HTTP_PROXY);
+    setGlobalDispatcher(globalThis.__undiciProxyAgent__);
+  }
+};
