b160363e008ebb0ce53080a98efbf8e14233aacc
Patrick Browne <pt.browne@gmail.com>
2025-04-01 15:29:27 +0200

    feat: Configure knip

--- knip.config.ts
+++ knip.config.ts
@@ -2,21 +2,33 @@ import { KnipConfig } from "knip";
 
 const config: KnipConfig = {
   entry: [
-    // Pages
-    "src/pages/**/*.ts",
-
     "codemods/*.js",
-
     "k6-load-test.ts",
-
-    "src/domain/gever/output/cli.js",
-
     "lingui.config.js",
-
+    "src/pages/**/*.ts",
+    "src/domain/gever/output/cli.js",
     "src/domain/gever/rollup.config.js",
   ],
   project: ["src/**", "scripts/**"],
   ignore: ["src/graphql/resolver-types.ts", "src/graphql/queries.ts"],
-  ignoreDependencies: ["eslint-config-next", "har-to-k6", "prettier"],
+  ignoreDependencies: [
+    // Added automatically by next
+    "eslint-config-next",
+
+    // Used through import type
+    "geojson",
+
+    // binary called directly from script
+    "har-to-k6",
+
+    // Can be useful
+    "prettier",
+
+    // Used through dynamic import
+    "swiss-maps",
+
+    // Used through webpack loaders
+    "raw-loader",
+  ],
 };
 export default config;
