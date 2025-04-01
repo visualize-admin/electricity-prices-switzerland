8462ed1833f1b07195c42dcb9922f80bbe99ec62
Patrick Browne <pt.browne@gmail.com>
2025-04-01 14:56:18 +0200

    feat: Configure knip

--- knip.config.ts
+++ knip.config.ts
@@ -7,11 +7,16 @@ const config: KnipConfig = {
 
     "codemods/*.js",
 
+    "k6-load-test.ts",
+
     "src/domain/gever/output/cli.js",
 
     "lingui.config.js",
+
+    "src/domain/gever/rollup.config.js",
   ],
   project: ["src/**", "scripts/**"],
   ignore: ["src/graphql/resolver-types.ts", "src/graphql/queries.ts"],
+  ignoreDependencies: ["eslint-config-next", "har-to-k6", "prettier"],
 };
 export default config;
