0f3d43caf35cedfb948b41258b7aab7251dbcb68
Patrick Browne <pt.browne@gmail.com>
2025-04-02 15:10:10 +0200

    fix: Remove unused lib (triggers problem when building on Vercel)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
     "@storybook/experimental-addon-test",
   ],
   framework: {
