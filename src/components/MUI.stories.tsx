95828bbc6aa1f60d3b12ab6ecd6d2fc282a17689
Patrick Browne <pt.browne@gmail.com>
2025-04-02 10:43:07 +0200

    refactor: Separate stories

---
+++ src/components/MUI.stories.tsx
@@ -0,0 +1,395 @@
+import {
+  Autocomplete,
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Chip,
+  Divider,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+import { IconAreaChart } from "src/icons/ic-area-chart";
+
+export const TypographyStory: React.FC = () => (
+  <Box sx={{ mb: 6 }}>
+    <Typography variant="h5" gutterBottom>
+      Typography Variants
+    </Typography>
+    <Divider sx={{ mb: 2 }} />
+    <Typography variant="h1">h1. Heading</Typography>
+    <Typography variant="h2">h2. Heading</Typography>
+    <Typography variant="h3">h3. Heading</Typography>
+    <Typography variant="h4">h4. Heading</Typography>
+    <Typography variant="h5">h5. Heading</Typography>
+    <Typography variant="h6">h6. Heading</Typography>
+    <Typography variant="subtitle1">
+      subtitle1. Lorem ipsum dolor sit amet
+    </Typography>
+    <Typography variant="subtitle2">
+      subtitle2. Lorem ipsum dolor sit amet
+    </Typography>
+    <Typography variant="body1">
+      body1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </Typography>
+    <Typography variant="body2">
+      body2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </Typography>
+    <Typography variant="button" display="block">
+      button text
+    </Typography>
+    <Typography variant="caption" display="block">
+      caption text
+    </Typography>
+    <Typography variant="overline" display="block">
+      overline text
+    </Typography>
+  </Box>
+);
+
+export const ButtonStory: React.FC = () => (
+  <Box sx={{ mb: 6 }}>
+    <Typography variant="h5" gutterBottom>
+      Button Variants
+    </Typography>
+    <Divider sx={{ mb: 2 }} />
+    <Stack spacing={2} direction="row" sx={{ mb: 2 }}>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+    <Typography variant="subtitle1" gutterBottom>
+      Colors
+    </Typography>
+    <Stack direction="row" spacing={1} sx={{ mb: 2 }}>
+      <Button color="primary" variant="contained">
+        Primary
+      </Button>
+      <Button color="secondary" variant="contained">
+        Secondary
+      </Button>
+      <Button color="success" variant="contained">
+        Success
+      </Button>
+      <Button color="error" variant="contained">
+        Error
+      </Button>
+      <Button color="info" variant="contained">
+        Info
+      </Button>
+      <Button color="warning" variant="contained">
+        Warning
+      </Button>
+    </Stack>
+    <Typography variant="subtitle1" gutterBottom>
+      Sizes
+    </Typography>
+    <Stack direction="row" spacing={1} alignItems="center">
+      <Button size="small">Small</Button>
+      <Button size="medium">Medium</Button>
+      <Button size="large">Large</Button>
+    </Stack>
+  </Box>
+);
+
+export const PaletteSection: React.FC = () => (
+  <Box sx={{ mb: 4 }}>
+    <Typography variant="h5" gutterBottom>
+      Palette
+    </Typography>
+    <Divider sx={{ mb: 2 }} />
+    <Grid container spacing={2}>
+      {["primary", "secondary", "error", "warning", "info", "success"].map(
+        (color) => (
+          <Grid item key={color} xs={6} sm={4} md={2}>
+            <Paper
+              sx={{
+                p: 2,
+                bgcolor: `${color}.main`,
+                color: `${color}.contrastText`,
+                textAlign: "center",
+              }}
+              elevation={3}
+            >
+              {color}
+            </Paper>
+            <Paper
+              sx={{
+                p: 2,
+                bgcolor: `${color}.light`,
+                color: "text.primary",
+                textAlign: "center",
+                mt: 1,
+              }}
+              elevation={1}
+            >
+              {color}.light
+            </Paper>
+            <Paper
+              sx={{
+                p: 2,
+                bgcolor: `${color}.dark`,
+                color: "white",
+                textAlign: "center",
+                mt: 1,
+              }}
+              elevation={1}
+            >
+              {color}.dark
+            </Paper>
+          </Grid>
+        )
+      )}
+    </Grid>
+  </Box>
+);
+
+export const FormStory: React.FC = () => (
+  <>
+    <Box sx={{ mb: 6 }}>
+      <Typography variant="h5" gutterBottom>
+        Input Fields
+      </Typography>
+      <Divider sx={{ mb: 2 }} />
+      <Stack spacing={3}>
+        <TextField label="Standard" variant="standard" />
+        <TextField label="Outlined" variant="outlined" />
+        <TextField label="Filled" variant="filled" />
+        <TextField
+          label="With Helper Text"
+          helperText="Some helper text"
+          variant="outlined"
+        />
+        <TextField
+          error
+          label="Error"
+          defaultValue="Invalid value"
+          helperText="Error message"
+          variant="outlined"
+        />
+        <TextField
+          label="Disabled"
+          disabled
+          defaultValue="Disabled input"
+          variant="outlined"
+        />
+      </Stack>
+    </Box>
+    <Box sx={{ mb: 6 }}>
+      <Typography variant="h5" gutterBottom>
+        Select
+      </Typography>
+      <Divider sx={{ mb: 2 }} />
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <FormControl fullWidth>
+            <InputLabel>Basic Select</InputLabel>
+            <Select value="option1" label="Basic Select">
+              <MenuItem value="option1">Option 1</MenuItem>
+              <MenuItem value="option2">Option 2</MenuItem>
+              <MenuItem value="option3">Option 3</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <FormControl fullWidth>
+            <InputLabel>Multiple Select</InputLabel>
+            <Select
+              multiple
+              value={["option1", "option2"]}
+              label="Multiple Select"
+              renderValue={(selected) => selected.join(", ")}
+            >
+              <MenuItem value="option1">Option 1</MenuItem>
+              <MenuItem value="option2">Option 2</MenuItem>
+              <MenuItem value="option3">Option 3</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+    </Box>
+    <Box sx={{ mb: 6 }}>
+      <Typography variant="h5" gutterBottom>
+        Autocomplete
+      </Typography>
+      <Divider sx={{ mb: 2 }} />
+      <Autocomplete
+        options={[
+          "JavaScript",
+          "TypeScript",
+          "React",
+          "Vue",
+          "Angular",
+          "Node.js",
+        ]}
+        renderInput={(params) => (
+          <TextField {...params} label="Programming languages" />
+        )}
+        sx={{ mb: 2 }}
+      />
+      <Autocomplete
+        multiple
+        options={[
+          "JavaScript",
+          "TypeScript",
+          "React",
+          "Vue",
+          "Angular",
+          "Node.js",
+        ]}
+        defaultValue={["TypeScript", "React"]}
+        renderInput={(params) => (
+          <TextField {...params} label="Multiple values" />
+        )}
+      />
+    </Box>
+  </>
+);
+
+export const ElevationStory: React.FC = () => (
+  <Box sx={{ mb: 6 }}>
+    <Typography variant="h5" gutterBottom>
+      Elevations
+    </Typography>
+    <Divider sx={{ mb: 2 }} />
+    <Grid container spacing={2}>
+      {[0, 1, 2, 3, 4, 6, 8, 12, 16, 24].map((elevation) => (
+        <Grid item key={elevation} xs={6} sm={4} md={2}>
+          <Paper
+            elevation={elevation}
+            sx={{
+              p: 2,
+              textAlign: "center",
+              height: "80px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            Elevation {elevation}
+          </Paper>
+        </Grid>
+      ))}
+    </Grid>
+  </Box>
+);
+
+export const CardStory: React.FC = () => (
+  <Box sx={{ mb: 6 }}>
+    <Typography variant="h5" gutterBottom>
+      Cards
+    </Typography>
+    <Divider sx={{ mb: 2 }} />
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card>
+          <CardHeader title="Card Title" subheader="September 14, 2023" />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              This is an example card with various elements you can include in a
+              MUI Card component.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography variant="h6" component="div">
+              Outlined Card
+            </Typography>
+            <Typography variant="body2">
+              A simpler card variant with an outline instead of elevation.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  </Box>
+);
+
+export const ChipStory: React.FC = () => (
+  <Box sx={{ mb: 6 }}>
+    <Typography variant="h5" gutterBottom>
+      Chips
+    </Typography>
+    <Divider sx={{ mb: 2 }} />
+    <Stack
+      direction="row"
+      spacing={1}
+      flexWrap="wrap"
+      useFlexGap
+      sx={{ mb: 2 }}
+    >
+      <Chip label="Basic" />
+      <Chip label="Disabled" disabled />
+      <Chip label="Clickable" onClick={() => {}} />
+      <Chip label="Deletable" onDelete={() => {}} />
+      <Chip avatar={<Avatar>M</Avatar>} label="With Avatar" />
+      <Chip icon={<IconAreaChart />} label="With Icon" />
+    </Stack>
+    <Typography variant="subtitle1" gutterBottom>
+      Colors
+    </Typography>
+    <Stack direction="row" spacing={1} flexWrap="wrap" useFlexGap>
+      <Chip label="Default" />
+      <Chip label="Primary" color="primary" />
+      <Chip label="Secondary" color="secondary" />
+      <Chip label="Success" color="success" />
+      <Chip label="Error" color="error" />
+      <Chip label="Info" color="info" />
+      <Chip label="Warning" color="warning" />
+    </Stack>
+  </Box>
+);
+
+export const KitchenSink = () => {
+  return (
+    <Box sx={{ p: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        MUI Kitchen Sink
+      </Typography>
+
+      {/* Typography Section */}
+      <TypographyStory />
+
+      {/* Button Section */}
+      <ButtonStory />
+
+      {/* Palette Section */}
+      <PaletteSection />
+
+      {/* Form Section */}
+      <FormStory />
+
+      {/* Elevation Section */}
+      <ElevationStory />
+
+      {/* Card Section */}
+      <CardStory />
+
+      {/* Chips Section */}
+      <ChipStory />
+    </Box>
+  );
+};
+
+export default {
+  component: KitchenSink,
+  title: "ui",
+};
