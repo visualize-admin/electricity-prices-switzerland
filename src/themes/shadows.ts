83ae14986829483b21a5b99948db21e7c0e18b5f
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-04-10 12:56:30 +0200

    fix: adjusted shadows to map our 6 to mui's 24
    @ptbrowne not sure if this is what you meant specifically with the shadow overrides?

--- src/themes/shadows.ts
+++ src/themes/shadows.ts
@@ -1,4 +1,7 @@
 import { e } from "@interactivethings/swiss-federal-ci";
 import { Shadows } from "@mui/material/styles/shadows";
 
-export const shadows = e as Shadows;
+export const shadows = Array.from({ length: 25 }, (_, i) => {
+  const index = Math.floor(i / 4);
+  return e[Math.min(index, e.length - 1)];
+}) as Shadows;
