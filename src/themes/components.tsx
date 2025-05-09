10eaed8db7c8b777fb647b368409b3b854394c7c
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-05-09 08:31:21 +0200

    fix: remove underline enforcement

--- src/themes/components.tsx
+++ src/themes/components.tsx
@@ -376,8 +376,6 @@ export const components: Components = {
     },
     styleOverrides: {
       root: ({ ownerState }) => {
-        const isPrimary = ownerState.color === "primary";
-
         const size = ownerState.size ?? "md";
         const sizeStyles = (() => {
           switch (size) {
@@ -398,7 +396,6 @@ export const components: Components = {
         return {
           ...sizeStyles,
           minWidth: 0,
-          textDecoration: isPrimary ? "underline" : "none",
         };
       },
     },
