cbb9832936e2fe8e27e6d6d97e9d5a341fc7a591
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-05-16 09:18:06 +0200

    fix: change custom impl. -> basicAuthMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,61 +1,24 @@
-import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { nextBasicAuthMiddleware } from "nextjs-basic-auth-middleware";
 
-import type { NextRequest } from "next/server";
-
-export const COOKIE_NAME = "preview_auth";
-
-const encoder = new TextEncoder();
-const SECRET = process.env.PREVIEW_COOKIE_SECRET!;
-if (!SECRET) throw new Error("Missing PREVIEW_COOKIE_SECRET");
-
-export async function isValidSignedPreviewCookie(
-  cookieValue?: string
-): Promise<boolean> {
-  if (!cookieValue) return false;
-
-  const [value, sig] = cookieValue.split(".");
-  if (!value || !sig) return false;
-
-  const key = await crypto.subtle.importKey(
-    "raw",
-    encoder.encode(SECRET),
-    { name: "HMAC", hash: "SHA-256" },
-    false,
-    ["sign"]
+const auth = (req: NextRequest) =>
+  nextBasicAuthMiddleware(
+    {
+      users: [
+        {
+          name: process.env.PREVIEW_USER!,
+          password: process.env.PREVIEW_PASSWORD!,
+        },
+      ],
+    },
+    req
   );
 
-  const expected = await crypto.subtle.sign("HMAC", key, encoder.encode(value));
-  const expectedHex = Buffer.from(expected).toString("hex");
-
-  return (
-    expectedHex.length === sig.length &&
-    expectedHex
-      .split("")
-      .reduce((acc, c, i) => acc | (c.charCodeAt(0) ^ sig.charCodeAt(i)), 0) ===
-      0
-  );
-}
-
-export async function middleware(req: NextRequest) {
-  //FIXME: scope for vercel preview only
-  const { pathname } = req.nextUrl;
-
+export function middleware(request: NextRequest) {
   if (
-    pathname.startsWith("/_next") ||
-    pathname.startsWith("/api") ||
-    pathname === "/favicon.ico" ||
-    pathname.match(/\.(.*)$/)
+    process.env.VERCEL_ENV === "preview" &&
+    request.nextUrl.pathname !== "/api/health"
   ) {
-    return NextResponse.next();
+    return auth(request);
   }
-
-  const cookie = req.cookies.get(COOKIE_NAME)?.value;
-  if (
-    (await isValidSignedPreviewCookie(cookie)) ||
-    pathname === "/_preview-auth"
-  ) {
-    return NextResponse.next();
-  }
-
-  return NextResponse.redirect(new URL("/_preview-auth", req.url));
 }
