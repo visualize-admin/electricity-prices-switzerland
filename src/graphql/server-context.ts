ee37ac43966197ecb79afc89327259f876d1cb92
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-05-16 09:23:18 +0200

    fix: fixed knip issues

--- src/graphql/server-context.ts
+++ src/graphql/server-context.ts
@@ -1,15 +1,5 @@
-import { parse } from "cookie";
-import { NextApiRequest } from "next";
-import { isValidSignedPreviewCookie } from "src/lib/auth";
-import { COOKIE_NAME } from "src/middleware";
+export type ServerContext = Record<string, never>;
 
-export interface ServerContext {
-  isAuthed: boolean;
-}
-
-export async function context(req: NextApiRequest): Promise<ServerContext> {
-  const cookies = parse(req.headers.cookie ?? "");
-  const cookie = cookies[COOKIE_NAME];
-  const isAuthed = isValidSignedPreviewCookie(cookie);
-  return { isAuthed };
-}
+export const context = async (): Promise<ServerContext> => {
+  return {};
+};
