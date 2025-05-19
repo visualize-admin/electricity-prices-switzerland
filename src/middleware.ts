0ef2b48fa26effb44d64671cb2dbb6bd2a670da1
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-05-19 14:48:39 +0200

    fix: fixed basic middleware auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,7 @@
-import { NextRequest } from "next/server";
-import { nextBasicAuthMiddleware } from "nextjs-basic-auth-middleware";
+import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
 
-const auth = (req: NextRequest) =>
-  nextBasicAuthMiddleware(
-    {
-      users: [
-        {
-          name: process.env.PREVIEW_USER!,
-          password: process.env.PREVIEW_PASSWORD!,
-        },
-      ],
-    },
-    req
-  );
+export const middleware = createNextAuthMiddleware({});
 
-export function middleware(request: NextRequest) {
-  if (
-    process.env.VERCEL_ENV === "preview" &&
-    request.nextUrl.pathname !== "/api/health"
-  ) {
-    return auth(request);
-  }
-}
+export const config = {
+  matcher: ["/(.*)"],
+};
