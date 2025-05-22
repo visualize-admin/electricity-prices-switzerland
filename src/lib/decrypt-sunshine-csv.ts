0968e4702021895847389781ce3af7a47f94929c
Patrick Browne <pt.browne@gmail.com>
2025-05-22 07:17:06 +0200

    feat: Decrypt sunshine csv returns a string instead of a buffer

--- src/lib/decrypt-sunshine-csv.ts
+++ src/lib/decrypt-sunshine-csv.ts
@@ -1,15 +1,16 @@
 import crypto from "crypto";
-import fs from "fs";
+import * as fs from "fs";
 import path from "path";
 
 import serverEnv from "src/env/server";
 
-const ENC_PATH = path.join(__dirname, "../sunshine-data.enc");
 const PASSWORD = serverEnv.PREVIEW_PASSWORD!;
 
-export const decryptSunshineCsv = (): Buffer => {
+export const decryptSunshineCsv = (): string => {
   try {
-    const encryptedData = fs.readFileSync(ENC_PATH);
+    const encryptedData = fs.readFileSync(
+      path.join(__dirname, "../sunshine-data.enc")
+    );
 
     const salt = encryptedData.subarray(0, 16);
     const iv = encryptedData.subarray(16, 32);
@@ -17,9 +18,11 @@ export const decryptSunshineCsv = (): Buffer => {
 
     const key = crypto.pbkdf2Sync(PASSWORD, salt, 100_000, 32, "sha256");
     const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
-    return Buffer.concat([decipher.update(data), decipher.final()]);
+    return Buffer.concat([decipher.update(data), decipher.final()]).toString(
+      "utf-8"
+    );
   } catch (e) {
     console.error("[Decrypt CSV Error]", e);
-    throw new Error("Failed to decrypt sunshine data.");
+    throw new Error(`Failed to decrypt sunshine data: ${e}`);
   }
 };
