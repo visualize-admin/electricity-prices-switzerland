ac06003ba0ce7d1e30cf2d57868c42715097df5d
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-05-26 10:03:00 +0200

    fix: remove duplicate contrainZoom func

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -58,58 +58,6 @@ const CH_BBOX: BBox = [
 ];
 
 /**
- * Constrain the viewState to always _contain_ the supplied bbox.
- *
- * (Other implementations ensure that the bbox _covers_ the viewport)
- *
- * @param viewState deck.gl viewState
- * @param bbox Bounding box of the feature to be contained
- */
-const constrainZoom = (
-  viewState: $FixMe,
-  bbox: BBox,
-  { padding }: { padding: number }
-) => {
-  if (viewState.width < padding * 2 || viewState.height < padding * 2) {
-    return viewState;
-  }
-
-  const vp = new WebMercatorViewport(viewState);
-
-  const { width, height, zoom, longitude, latitude } = viewState;
-
-  const [x, y] = vp.project([longitude, latitude]);
-  const [x0, y1] = vp.project(bbox[0]);
-  const [x1, y0] = vp.project(bbox[1]);
-
-  const fitted = vp.fitBounds(bbox, { padding });
-
-  const [cx, cy] = vp.project([fitted.longitude, fitted.latitude]);
-
-  const h = height - padding * 2;
-  const w = width - padding * 2;
-
-  const h2 = h / 2;
-  const w2 = w / 2;
-
-  const y2 =
-    y1 - y0 < h ? cy : y - h2 < y0 ? y0 + h2 : y + h2 > y1 ? y1 - h2 : y;
-  const x2 =
-    x1 - x0 < w ? cx : x - w2 < x0 ? x0 + w2 : x + w2 > x1 ? x1 - w2 : x;
-
-  const p = vp.unproject([x2, y2]);
-
-  return {
-    ...viewState,
-    transitionDuration: 0,
-    transitionInterpolator: null,
-    zoom: Math.max(zoom, fitted.zoom),
-    longitude: p[0],
-    latitude: p[1],
-  };
-};
-
-/**
  * Simple fitZoom to bbox
  * @param viewState deck.gl viewState
  */
