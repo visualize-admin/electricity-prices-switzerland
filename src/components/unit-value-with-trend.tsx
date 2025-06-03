8180c2245857bd730df19f94b990772544da8891
Patrick Browne <pt.browne@gmail.com>
2025-06-03 08:40:46 +0200

    refactor: Extract TrendIcon and use correct icon

--- src/components/unit-value-with-trend.tsx
+++ src/components/unit-value-with-trend.tsx
@@ -1,20 +1,17 @@
 import { Box, Typography } from "@mui/material";
 
+import { TrendIcon } from "src/components/TrendIcon";
+
 const UnitValueWithTrend: React.FC<{
   value: number;
   unit: string;
   trend: "stable" | "increasing" | "decreasing";
 }> = ({ value, unit, trend }) => {
-  // TODO
-  const trendIcon = {
-    stable: "↔️",
-    increasing: "📈",
-    decreasing: "📉",
-  }[trend];
-
   return (
     <Box sx={{ display: "inline-flex", alignItems: "baseline", gap: 1 }}>
-      <span>{trendIcon}</span>
+      <span>
+        <TrendIcon trend={trend} />
+      </span>
       <Typography
         variant="h3"
         color="text.primary"
