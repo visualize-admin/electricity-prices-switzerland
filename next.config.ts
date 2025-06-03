95c3f95e81b41513d97705bc80bcc22b0623158e
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-06-03 14:14:02 +0200

    fix: add back fs resolves

--- next.config.ts
+++ next.config.ts
@@ -79,7 +79,12 @@ const nextConfig = async (): Promise<NextConfig> => {
       localeDetection: false,
     },
 
-    webpack(config) {
+    webpack(config, { isServer }) {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.resolve.fallback = { fs: false };
+      }
+
       config.module.rules.push({
         test: /\.(graphql|gql)$/,
         exclude: /node_modules/,
