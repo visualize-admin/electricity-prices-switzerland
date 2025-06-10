046602890717985f869872a0bb4266b102bd56af
Patrick Browne <pt.browne@gmail.com>
2025-06-10 13:05:12 +0200

    feat: Graphql queries are executed server side and urql cache populated client side

--- src/graphql/context.tsx
+++ src/graphql/context.tsx
@@ -1,8 +1,11 @@
 import { ReactNode } from "react";
 import { createClient, Provider } from "urql";
+import { exchanges } from "./urql-exchanges";
 
 const client = createClient({
   url: "/api/graphql",
+  exchanges,
+  suspense: true,
 });
 
 export const GraphqlProvider = ({ children }: { children: ReactNode }) => {
