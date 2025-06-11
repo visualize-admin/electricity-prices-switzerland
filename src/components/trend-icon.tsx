d697b7928f65152c834995266dcd09a0f516be62
Patrick Browne <pt.browne@gmail.com>
2025-06-11 11:41:03 +0200

    fix: Disable icon for "stable"

--- src/components/trend-icon.tsx
+++ src/components/trend-icon.tsx
@@ -4,7 +4,7 @@ import { Icon } from "src/icons";
 
 type Trend = "stable" | "increasing" | "decreasing";
 const iconName = {
-  stable: "minus",
+  stable: null,
   increasing: "arrowup",
   decreasing: "arrowdown",
 } as const;
@@ -12,5 +12,5 @@ export const TrendIcon: React.FC<
   { trend: Trend } & Omit<ComponentProps<typeof Icon>, "name">
 > = ({ trend, ...props }) => {
   const icon = iconName[trend];
-  return <Icon {...props} name={icon} />;
+  return icon ? <Icon {...props} name={icon} /> : null;
 };
