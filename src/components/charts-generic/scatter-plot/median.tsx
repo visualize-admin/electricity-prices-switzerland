5161caf27c4a2ca7f485a7691a4332da06d78c9e
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-06-11 09:20:37 +0200

    fix: yScale median
    https://github.com/visualize-admin/electricity-prices-switzerland/pull/224#discussion_r2137678153

--- src/components/charts-generic/scatter-plot/median.tsx
+++ src/components/charts-generic/scatter-plot/median.tsx
@@ -5,28 +5,33 @@ import { useChartState } from "../use-chart-state";
 import { ScatterPlotState } from "./scatter-plot-state";
 
 export const ScatterPlotMedian = () => {
-  const {
-    medianValue: m,
-    bounds,
-    xScale,
-  } = useChartState() as ScatterPlotState;
+  const { medianValue, bounds, xScale, yScale } =
+    useChartState() as ScatterPlotState;
 
   const diamondSize = 8;
 
-  if (!m) {
+  if (!medianValue) {
     return null;
   }
 
-  const x = xScale(m) + bounds.margins.left;
-  const y = bounds.chartHeight / 2 + bounds.margins.top;
+  const x = xScale(medianValue);
+
   return (
-    <rect
-      x={x - diamondSize / 2}
-      y={y - diamondSize / 2}
-      width={diamondSize}
-      height={diamondSize}
-      color={palette.monochrome[800]}
-      transform={`rotate(45, ${x}, ${y})`}
-    />
+    <g transform={`translate(${bounds.margins.left}, ${bounds.margins.top})`}>
+      {yScale.domain().map((yValue) => {
+        const y = (yScale(yValue) || 0) + yScale.bandwidth() / 2;
+        return (
+          <rect
+            key={yValue}
+            x={x - diamondSize / 2}
+            y={y - diamondSize / 2}
+            width={diamondSize}
+            height={diamondSize}
+            fill={palette.monochrome[800]}
+            transform={`rotate(45, ${x}, ${y})`}
+          />
+        );
+      })}
+    </g>
   );
 };
