52a83cfdc23fff10a2347efafa311bb3592b739c
Patrick Browne <pt.browne@gmail.com>
2025-06-11 15:11:27 +0200

    feat: Activate basic auth credentials for playwright e2e and take screenshot

--- e2e/sunshine.spec.ts
+++ e2e/sunshine.spec.ts
@@ -6,10 +6,14 @@ test.describe("Sunshine details page", () => {
   test("it should be possible to load a sunshine details page", async ({
     withFlag,
     page,
+    snapshot,
   }) => {
     const resp = await page.goto(
       withFlag("/sunshine/operator/426/costs-and-tariffs", { sunshine: true })
     );
     await expect(resp?.status()).toEqual(200);
+    await snapshot({
+      note: "Sunshine details page",
+    });
   });
 });
