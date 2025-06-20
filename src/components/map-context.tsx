ffe6a842cea8ad5eaf68f7146969d1a280b7a10b
Patrick Browne <pt.browne@gmail.com>
2025-06-20 11:31:02 +0200

    refactor: List click handled via provider

--- src/components/map-context.tsx
+++ src/components/map-context.tsx
@@ -1,19 +1,27 @@
+import { useRouter } from "next/router";
 import {
   createContext,
   Dispatch,
   ReactNode,
   SetStateAction,
   useContext,
+  useMemo,
   useState,
 } from "react";
 
 import { Entity } from "src/domain/data";
+import useEvent from "src/lib/use-event";
+import { useFlag } from "src/utils/flags";
 
 type MapContextType = {
   activeId: string | null;
   setActiveId: Dispatch<SetStateAction<string | null>>;
   entity: Entity;
   setEntity: Dispatch<SetStateAction<Entity>>;
+  onListItemSelect: (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    id: string
+  ) => void;
 };
 
 const MapContext = createContext<MapContextType | undefined>(undefined);
@@ -30,14 +38,31 @@ export const MapProvider = ({
   children,
 }: MapProviderProps) => {
   const [entity, setEntity] = useState<Entity>("municipality");
+  const isSunshine = useFlag("sunshine");
+  const router = useRouter();
 
-  return (
-    <MapContext.Provider
-      value={{ activeId, setActiveId, entity: entity, setEntity: setEntity }}
-    >
-      {children}
-    </MapContext.Provider>
+  const onListItemSelect: MapContextType["onListItemSelect"] = useEvent(
+    (_, id: string) => {
+      if (isSunshine) {
+        setActiveId(id);
+      } else {
+        router.push(`/${entity}/${id}`);
+      }
+    }
   );
+
+  const value = useMemo(
+    () => ({
+      activeId,
+      setActiveId,
+      entity,
+      setEntity,
+      onListItemSelect,
+    }),
+    [activeId, setActiveId, entity, setEntity, onListItemSelect]
+  );
+
+  return <MapContext.Provider value={value}>{children}</MapContext.Provider>;
 };
 
 export const useMap = (): MapContextType => {
