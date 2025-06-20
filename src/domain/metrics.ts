48fe4bb35f909f161a1bb3e2e195c453a241996f
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-06-20 08:47:13 +0200

    chore: added translations to the units

--- src/domain/metrics.ts
+++ src/domain/metrics.ts
@@ -1,8 +1,19 @@
+import { t } from "@lingui/macro";
+
 import { NetworkLevel } from "./data";
 
-export const RP_PER_KWH = "Rp./kWh";
-export const RP_PER_KM = "Rp./km";
-export const MIN_PER_YEAR = "Min./year";
+export const RP_PER_KWH = t({
+  id: "sunshine.metric-unit.rp-kwh",
+  message: "Rp./kWh",
+});
+export const RP_PER_KM = t({
+  id: "sunshine.metric-unit.rp-km",
+  message: "Rp./km",
+});
+export const MIN_PER_YEAR = t({
+  id: "sunshine.metric-unit.min-year",
+  message: "Min./year",
+});
 
 export const getNetworkLevelMetrics = (level: NetworkLevel["id"]) => {
   switch (level) {
