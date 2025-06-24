0224eb52c1404f2ab42acc0cf8a775110d122ea5
Patrick Browne <pt.browne@gmail.com>
2025-06-24 10:30:46 +0200

    feat: Add energy tariffs adapter

--- src/components/map-details-chart-adapters.tsx
+++ src/components/map-details-chart-adapters.tsx
@@ -8,7 +8,7 @@ import {
   QueryStateSunshineIndicator,
   useQueryStateSunshineMap,
 } from "src/domain/query-states";
-import { useNetTariffsQuery } from "src/graphql/queries";
+import { useEnergyTariffsQuery, useNetTariffsQuery } from "src/graphql/queries";
 
 import { ListItemType } from "./list";
 
@@ -68,11 +68,68 @@ const NetTariffsChartAdapter = ({
   );
 };
 
+const EnergyTariffsChartAdapter = ({
+  period,
+  selectedItem,
+}: {
+  period: string;
+  selectedItem: ListItemType;
+}) => {
+  const { id, label: operatorLabel } = selectedItem;
+  const [queryState] = useQueryStateSunshineMap();
+  const { energyTariffCategory } = queryState;
+  const [{ data, fetching }] = useEnergyTariffsQuery({
+    variables: {
+      filter: {
+        operatorId: parseInt(id, 10),
+        period: parseInt(period, 10),
+        category: energyTariffCategory,
+      },
+    },
+  });
+
+  // TODO Do the filtering to get only operator observations at
+  // graphql level
+  const yearlyData = useMemo(() => {
+    const operatorId = parseInt(id, 10);
+    return data?.energyTariffs.yearlyData.filter(
+      (p) => p.operator_id === operatorId
+    );
+  }, [data?.energyTariffs.yearlyData, id]);
+
+  if (fetching) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <Typography variant="h6" fontWeight="bold" mb={2}>
+        <Trans id="sunshine.costs-and-tariffs.energy-tariffs-trend.title">
+          Energy Tariffs Trend
+        </Trans>
+      </Typography>
+      <NetTariffsTrendChart
+        id={id}
+        observations={yearlyData ?? []}
+        netTariffs={{
+          category: energyTariffCategory,
+          peerGroupMedianRate: data?.energyTariffs?.peerGroupMedianRate,
+        }}
+        operatorLabel={operatorLabel ?? ""}
+        view="progress"
+        compareWith={[]}
+        mini
+      />
+    </div>
+  );
+};
+
 export const indicatorToChart: Partial<
   Record<
     QueryStateSunshineIndicator,
     React.FC<{ period: string; selectedItem: ListItemType }>
   >
 > = {
+  energyTariffs: EnergyTariffsChartAdapter,
   netTariffs: NetTariffsChartAdapter,
 } as const;
