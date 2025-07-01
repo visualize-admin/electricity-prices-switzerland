e65bcf1a9c128164549f2f8409444105448f0305
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-07-01 13:10:22 +0200

    feat: extend comparison card

--- src/components/table-comparison-card.tsx
+++ src/components/table-comparison-card.tsx
@@ -1,11 +1,12 @@
 import {
-  Typography,
   Card,
   CardContent,
-  TableBody,
-  TableRow,
-  TableCell,
   CardProps,
+  Stack,
+  TableBody,
+  TableCell,
+  TableRow,
+  Typography,
 } from "@mui/material";
 import React from "react";
 
@@ -13,10 +14,14 @@ import ComparisonTable from "src/components/comparison-table";
 import UnitValueWithTrend from "src/components/unit-value-with-trend";
 import { Trend } from "src/graphql/resolver-types";
 
+import { YearlyNavigation, YearlyNavigationProps } from "./sunshine-tabs";
+
 const TableComparisonCard: React.FC<
   {
     title: React.ReactNode;
     subtitle: React.ReactNode;
+
+    linkContent?: React.ReactNode;
     rows: {
       label: React.ReactNode;
       value:
@@ -28,16 +33,33 @@ const TableComparisonCard: React.FC<
           }
         | { value: React.ReactElement | string };
     }[];
-  } & Omit<CardProps, "title" | "subtitle" | "rows">
-> = ({ title, subtitle, rows, ...props }) => (
+  } & Omit<CardProps, "title" | "subtitle" | "rows"> &
+    Partial<YearlyNavigationProps>
+> = ({
+  title,
+  subtitle,
+  rows,
+  linkContent,
+  activeTab,
+  handleTabChange,
+  ...props
+}) => (
   <Card {...props}>
     <CardContent>
       <Typography variant="h3" gutterBottom>
         {title}
       </Typography>
-      <Typography variant="subtitle2" color="text.  secondary" gutterBottom>
-        {subtitle}
-      </Typography>
+      {(!activeTab || !handleTabChange) && (
+        <Typography variant="subtitle2" color="text.secondary" gutterBottom>
+          {subtitle}
+        </Typography>
+      )}
+      {activeTab && handleTabChange && (
+        <YearlyNavigation
+          activeTab={activeTab}
+          handleTabChange={handleTabChange}
+        />
+      )}
       <ComparisonTable size="small">
         <TableBody>
           {rows.map((row, index) => (
@@ -63,6 +85,17 @@ const TableComparisonCard: React.FC<
           ))}
         </TableBody>
       </ComparisonTable>
+      <Stack
+        sx={{
+          mt: 2,
+          flexGrow: 1,
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          alignItems: "flex-end",
+        }}
+      >
+        {linkContent}
+      </Stack>
     </CardContent>
   </Card>
 );
