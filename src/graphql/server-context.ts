7be4e0dd59d0c773c17a4460c7b32c913fb9173b
Patrick Browne <pt.browne@gmail.com>
2025-07-04 17:30:38 +0200

    fix: Remove duplicated logic for selection of sunshine data service

--- src/graphql/server-context.ts
+++ src/graphql/server-context.ts
@@ -1,35 +1,13 @@
 import { NextApiRequest } from "next";
-
-import { sunshineDataService as sqlSunshineDataService } from "src/lib/db/sql";
-import { SunshineDataService } from "src/lib/sunshine-database-service";
-
-const DATABASE_SERVICE_MAP: Record<string, SunshineDataService> = {
-  sql: sqlSunshineDataService,
-};
-
-const DEFAULT_DATABASE_SERVICE_KEY = "sql";
-
-function getSunshineDataService(req: NextApiRequest): SunshineDataService {
-  const serviceKey = req.headers["x-database-service"] as string;
-
-  if (!serviceKey || !DATABASE_SERVICE_MAP[serviceKey]) {
-    console.log(
-      "Using default database service:",
-      DEFAULT_DATABASE_SERVICE_KEY
-    );
-    return DATABASE_SERVICE_MAP[DEFAULT_DATABASE_SERVICE_KEY];
-  }
-
-  console.log("Using database service:", DEFAULT_DATABASE_SERVICE_KEY);
-  return DATABASE_SERVICE_MAP[serviceKey];
-}
+import { SunshineDataService } from "src/lib/sunshine-data-service";
+import { getSunshineDataServiceFromApiRequest } from "src/lib/sunshine-data-service-context";
 
 export type ServerContext = {
   sunshineDataService: SunshineDataService;
 };
 
 export const context = async (req: NextApiRequest): Promise<ServerContext> => {
-  const sunshineDataService = getSunshineDataService(req);
+  const sunshineDataService = getSunshineDataServiceFromApiRequest(req);
 
   return {
     sunshineDataService,
