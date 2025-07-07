93533215a955fb4f95254266e6d03726e278be88
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-07-07 10:36:14 +0200

    fix: knip

--- src/components/charts-generic/constants.ts
+++ src/components/charts-generic/constants.ts
@@ -8,7 +8,3 @@ export const BAR_HEIGHT = 24;
 export const BAR_HEIGHT_SMALL = 18;
 export const BAR_PADDING = 16;
 export const LABEL_PADDING = 16;
-
-// Histogram chart
-export const TOOLTIP_ARROW_HEIGHT = 8;
-export const VERTICAL_TICK_OFFSET = 19;
