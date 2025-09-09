3711b89e70773149eed20ef16552069b4f4b9a6d
Patrick Browne <pt.browne@gmail.com>
2025-09-09 11:13:02 +0200

    refactor: Move matomo id to runtime env

--- src/pages/api/matomo-id.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-import serverEnv from "src/env/server";
-import assert from "src/lib/assert";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  assert(!!serverEnv, "serverEnv is not defined");
-  res.json({ matomoId: serverEnv.MATOMO_ID });
-};
