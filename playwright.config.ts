aed3dc003643a08997075c1e9cf19e72ac3604b3
Patrick Browne <pt.browne@gmail.com>
2025-09-18 11:29:27 +0200

    feat: Add list reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,12 +15,6 @@ const getHttpCredentialsFromEnv = () => {
 };
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
@@ -34,7 +28,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["list"], ["html"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
