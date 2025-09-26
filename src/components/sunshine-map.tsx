baee7f08fc7ed8b8b914b9e0873ac4b5fea88f41
Patrick Browne <pt.browne@gmail.com>
2025-09-26 14:40:32 +0200

    fix: Remove features with no values otherwise municipalities have no background

--- src/components/sunshine-map.tsx
+++ src/components/sunshine-map.tsx
@@ -145,17 +145,12 @@ const SunshineMap = ({
     );
 
     const features = operatorsFeatureCollection.features;
-    const filteredFeatures = features.filter((feature) => {
-      return feature.properties.operators.some(
-        (opId) => !!(observationsByOperator[opId] ?? {}).value
-      );
-    });
 
     return {
       ...operatorsFeatureCollection,
-      features: filteredFeatures,
+      features,
     };
-  }, [enrichedData?.operatorMunicipalities, geoData, observationsByOperator]);
+  }, [enrichedData?.operatorMunicipalities, geoData]);
 
   // Entity selection state
   const [entitySelection, setEntitySelection] = useState<EntitySelection>({
