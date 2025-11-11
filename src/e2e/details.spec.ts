7b7608670c7f07e4466bc1197dbfd9fbe6d9c235
Patrick Browne <pt.browne@gmail.com>
2025-11-11 20:58:06 +0100

    feat: Always run on /en

--- src/e2e/details.spec.ts
+++ src/e2e/details.spec.ts
@@ -4,7 +4,7 @@ test.describe("Segmented control visibility", () => {
   test("should appear for operator with multiple municipalities", async ({
     page,
   }) => {
-    await page.goto("/operator/565?period=2025");
+    await page.goto("/en/operator/565?period=2025");
     await page.getByRole("heading", { name: "Price components" }).waitFor();
     await expect(
       await page.getByRole("button", { name: "Group municipalities" })
@@ -14,7 +14,7 @@ test.describe("Segmented control visibility", () => {
   test("should not appear for operator with single municipality", async ({
     page,
   }) => {
-    await page.goto("/operator/818");
+    await page.goto("/en/operator/818");
     await page.getByRole("heading", { name: "Price components" }).waitFor();
 
     await expect(
@@ -25,7 +25,7 @@ test.describe("Segmented control visibility", () => {
   test("should not appear for municipality view with a single operator", async ({
     page,
   }) => {
-    await page.goto("/municipality/261?period=2025");
+    await page.goto("/en/municipality/261?period=2025");
     await page.getByRole("heading", { name: "Price components" }).waitFor();
 
     await expect(
@@ -36,7 +36,7 @@ test.describe("Segmented control visibility", () => {
   test("should appear for municipality view with multiple operators", async ({
     page,
   }) => {
-    await page.goto("/municipality/619?period=2025");
+    await page.goto("/en/municipality/619?period=2025");
     await page.getByRole("heading", { name: "Price components" }).waitFor();
 
     await expect(
@@ -45,7 +45,7 @@ test.describe("Segmented control visibility", () => {
   });
 
   test("should not appear for canton view", async ({ page }) => {
-    await page.goto("/canton/1");
+    await page.goto("/en/canton/1");
     await expect(
       page.getByRole("button", { name: "Group municipalities" })
     ).not.toBeVisible();
