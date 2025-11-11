f1334514a8eeeb928a4a0745bc04274d93ff0ab6
Patrick Browne <pt.browne@gmail.com>
2025-11-11 12:37:21 +0100

    refactor: Make the prepare observations less confusing

--- src/components/detail-page/price-components-bars-utils.ts
+++ src/components/detail-page/price-components-bars-utils.ts
@@ -7,6 +7,55 @@ import { PriceComponent } from "src/graphql/queries";
 
 export const EXPANDED_TAG = "expanded";
 
+const addDynamicTariffs = (obj: Record<string, ObservationValue | $FixMe>) => ({
+  ...obj,
+  max: 65.45,
+  min: 12.89,
+});
+
+const createSingleObservation = (obs: Record<string, ObservationValue>) => ({
+  ...obs,
+  label: obs.uniqueId,
+});
+
+const createGroupedObservation = (
+  group: [ObservationValue, Record<string, ObservationValue>[]],
+  priceComponent: PriceComponent,
+  entity: Entity
+) => {
+  const [groupValue, observations] = group;
+  const firstObs = observations[0];
+  return {
+    priceComponent,
+    value: groupValue,
+    [entity]: firstObs[entity],
+    period: firstObs.period,
+    uniqueId: `${priceComponent}${firstObs.period}${firstObs.operatorLabel}${firstObs.municipalityLabel}${observations.length}`,
+    label: `${firstObs.period}, ${firstObs.operatorLabel}, ${
+      observations.length
+    } ${getLocalizedLabel({
+      id: entity === "operator" ? "municipalities" : "operators",
+    })}`,
+    entities: group[1],
+  };
+};
+
+const createExpandedObservation = (
+  d: Record<string, ObservationValue>,
+  group: [ObservationValue, Record<string, ObservationValue>[]],
+  priceComponent: PriceComponent,
+  entity: Entity
+) => {
+  return {
+    priceComponent,
+    value: d.value,
+    [entity]: d[entity],
+    period: d.period,
+    uniqueId: `${priceComponent}${d.period}${d.operatorLabel}${d.municipalityLabel}${group[1].length}${EXPANDED_TAG}`,
+    label: entity === "municipality" ? d.operatorLabel : d.municipalityLabel,
+  };
+};
+
 export const prepareObservations = ({
   groupedObservations,
   priceComponent,
@@ -26,83 +75,33 @@ export const prepareObservations = ({
   view: string;
   dynamicTariffsFlag: FlagValue;
 }) => {
-  if (entity === "canton") {
-    return groupedObservations.flatMap((year) =>
-      year[1].flatMap((ent) =>
-        ent[1].flatMap((value) => ({
-          ...value[1][0],
-          ...(dynamicTariffsFlag ? { max: 65.45, min: 12.89 } : {}),
-          label: value[1][0].uniqueId,
-        }))
-      )
-    );
-  } else {
-    return view === "collapsed"
-      ? groupedObservations.flatMap((year) =>
-          year[1].flatMap((ent) =>
-            ent[1].flatMap((value) =>
-              value[1].length === 1
-                ? {
-                    ...value[1][0],
-                    label: value[1][0].uniqueId,
-                    ...(dynamicTariffsFlag ? { max: 65.45, min: 12.89 } : {}),
-                  }
-                : {
-                    priceComponent,
-                    value: value[0],
-                    ...(dynamicTariffsFlag ? { max: 65.45, min: 12.89 } : {}),
-                    [entity]: value[1][0][entity],
-                    period: value[1][0].period,
-                    uniqueId: `${priceComponent}${value[1][0].period}${value[1][0].operatorLabel}${value[1][0].municipalityLabel}${value[1].length}`,
-                    label: `${value[1][0].period}, ${
-                      value[1][0].operatorLabel
-                    }, ${value[1].length} ${getLocalizedLabel({
-                      id:
-                        entity === "operator" ? "municipalities" : "operators",
-                    })}`,
-                    entities: value[1],
-                  }
-            )
-          )
-        )
-      : groupedObservations.flatMap((year) =>
-          year[1].flatMap((ent) =>
-            ent[1].flatMap((value) => {
-              const singleEntities = value[1]
-                .flatMap((d) => ({
-                  priceComponent,
-                  value: d.value,
-                  ...(dynamicTariffsFlag ? { max: 65.45, min: 12.89 } : {}),
-                  [entity]: d[entity],
-                  period: d.period,
-                  uniqueId: `${priceComponent}${d.period}${d.operatorLabel}${d.municipalityLabel}${value[1].length}${EXPANDED_TAG}`,
-                  label:
-                    entity === "municipality"
-                      ? d.operatorLabel
-                      : d.municipalityLabel,
-                }))
-                .sort((a, b) => ascending(a.label, b.label));
-              const groupPlusSingleValues = [
-                {
-                  priceComponent,
-                  value: value[0],
-                  ...(dynamicTariffsFlag ? { max: 65.45, min: 12.89 } : {}),
-                  [entity]: value[1][0][entity],
-                  period: value[1][0].period,
-                  uniqueId: `${priceComponent}${value[1][0].period}${value[1][0].operatorLabel}${value[1][0].municipalityLabel}${value[1].length}`,
-                  label: `${value[1][0].period}, ${
-                    value[1][0].operatorLabel
-                  }, ${value[1].length} ${getLocalizedLabel({
-                    id: entity === "operator" ? "municipalities" : "operators",
-                  })}`,
-                  entities: value[1],
-                },
-                ...singleEntities,
-              ];
+  return groupedObservations.flatMap((year) =>
+    year[1].flatMap((ent) =>
+      ent[1]
+        .flatMap((group) => {
+          const [, observations] = group;
+          if (entity === "canton") {
+            return createSingleObservation(observations[0]);
+          }
 
-              return groupPlusSingleValues;
-            })
-          )
-        );
-  }
+          if (view === "collapsed") {
+            return observations.length === 1
+              ? createSingleObservation(observations[0])
+              : createGroupedObservation(group, priceComponent, entity);
+          }
+
+          const singleEntities = observations
+            .map((d) =>
+              createExpandedObservation(d, group, priceComponent, entity)
+            )
+            .sort((a, b) => ascending(a.label, b.label));
+
+          return [
+            createGroupedObservation(group, priceComponent, entity),
+            ...singleEntities,
+          ];
+        })
+        .map((obs) => (dynamicTariffsFlag ? addDynamicTariffs(obs) : obs))
+    )
+  );
 };
