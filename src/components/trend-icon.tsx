67df33a6910114278c1d2182f777d58ead188485
Patrick Browne <pt.browne@gmail.com>
2026-01-06 10:59:55 +0100

    feat: Use smaller and correctly aligned arrow

--- src/components/trend-icon.tsx
+++ src/components/trend-icon.tsx
@@ -1,19 +1,39 @@
 import { ComponentProps } from "react";
+import { makeStyles } from "tss-react/mui";
 
 import { Trend } from "src/graphql/resolver-types";
 import { Icon } from "src/icons";
+
+const useStyles = makeStyles()(() => ({
+  icon: {
+    width: 12,
+    height: 12,
+  },
+}));
 
 const iconName = {
   [Trend.Stable]: null,
   [Trend.Up]: "arrowup",
   [Trend.Down]: "arrowdown",
 } as const;
+
 export const TrendIcon: React.FC<
   { trend: Trend | undefined | null } & Omit<
     ComponentProps<typeof Icon>,
     "name"
   >
 > = ({ trend, ...props }) => {
+  const { classes, cx } = useStyles();
   const icon = trend ? iconName[trend] : undefined;
-  return icon ? <Icon {...props} name={icon} /> : null;
+
+  return icon ? (
+    <Icon
+      {...props}
+      name={icon}
+      className={cx(classes.icon, props.className)}
+      viewBox="4 4 16 16"
+      width="12"
+      height="12"
+    />
+  ) : null;
 };
