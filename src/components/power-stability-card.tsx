f125da05bd6762ec695ad61a6def8cd8c03e05cf
Patrick Browne <pt.browne@gmail.com>
2026-01-08 16:34:24 +0100

    fix: Extra margin

--- src/components/power-stability-card.tsx
+++ src/components/power-stability-card.tsx
@@ -190,19 +190,14 @@ export const PowerStabilityCard: React.FC<PowerStabilityCardProps> = (
           <Typography variant="h3" gutterBottom>
             {props.cardTitle}
           </Typography>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            gutterBottom
-            mb={8}
-          >
+          <Typography variant="body2" color="text.secondary" gutterBottom>
             <Trans id="sunshine.costs-and-tariffs.benchmarking-peer-group">
               Benchmarking within the Peer Group: {peerGroupLabel}
             </Trans>
           </Typography>
         </CardHeader>
         <Grid container spacing={3} sx={{ mb: 3 }}>
-          <Grid item xs={12} sm={4} sx={{ mt: 2.5 }}>
+          <Grid item xs={12} sm={4}>
             <ButtonGroup
               id="view-by-button-group-1"
               label={getLocalizedLabel({
