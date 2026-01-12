fdfac1faabb9fab3a12c97e57a4f8418653908b9
Patrick Browne <pt.browne@gmail.com>
2026-01-12 14:07:14 +0100

    refactor: Use useStyles instead of box sx

--- src/components/sunshine/sunshine-card.tsx
+++ src/components/sunshine/sunshine-card.tsx
@@ -1,54 +1,48 @@
 import { Box, Typography } from "@mui/material";
 import { ReactNode } from "react";
+import { makeStyles } from "tss-react/mui";
 
 type SunshineCardProps = {
   children: ReactNode;
 };
 
-export const SunshineCard = (props: SunshineCardProps) => {
-  const { children } = props;
-  return (
-    <Box
-      sx={{
-        flexDirection: "column",
-        height: "100%",
-        backgroundColor: "background.paper",
-        boxShadow: 3,
-        borderRadius: 1,
-      }}
-      display={"flex"}
-    >
-      {children}
-    </Box>
-  );
+const useStyles = makeStyles()((theme) => ({
+  card: {
+    flexDirection: "column",
+    height: "100%",
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[3],
+    borderRadius: theme.shape.borderRadius,
+    display: "flex",
+  },
+  header: {
+    padding: theme.spacing(6.5, 6),
+  },
+  content: {
+    padding: theme.spacing(0, 6.5, 6.5, 6.5),
+  },
+}));
+
+type SunshineCardPropsWithClassName = SunshineCardProps & {
+  className?: string;
 };
 
-export const SunshineCardHeader = (props: SunshineCardProps) => {
-  const { children } = props;
-  return (
-    <Box
-      sx={{
-        px: 6,
-        py: 6.5,
-      }}
-    >
-      {children}
-    </Box>
-  );
+export const SunshineCard = (props: SunshineCardPropsWithClassName) => {
+  const { children, className } = props;
+  const { classes, cx } = useStyles();
+  return <div className={cx(classes.card, className)}>{children}</div>;
 };
 
-export const SunshineCardContent = (props: SunshineCardProps) => {
-  const { children } = props;
-  return (
-    <Box
-      sx={{
-        p: 6.5,
-        pt: 0,
-      }}
-    >
-      {children}
-    </Box>
-  );
+export const SunshineCardHeader = (props: SunshineCardPropsWithClassName) => {
+  const { children, className } = props;
+  const { classes, cx } = useStyles();
+  return <div className={cx(classes.header, className)}>{children}</div>;
+};
+
+export const SunshineCardContent = (props: SunshineCardPropsWithClassName) => {
+  const { children, className } = props;
+  const { classes, cx } = useStyles();
+  return <div className={cx(classes.content, className)}>{children}</div>;
 };
 
 export const SunshineCardTitle = (props: SunshineCardProps) => {
