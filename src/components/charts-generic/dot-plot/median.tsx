f5a134919a8d0d03d6ff18ad6de6b8a805619573
Patrick Browne <pt.browne@gmail.com>
2026-01-12 14:43:35 +0100

    refactor: Use MedianDiamond component

--- src/components/charts-generic/dot-plot/median.tsx
+++ src/components/charts-generic/dot-plot/median.tsx
@@ -1,6 +1,5 @@
-import { palette } from "src/themes/palette";
+import { MedianDiamond } from "src/components/charts-generic/dot-plot/median-diamond.tsx";
 
-import { MEDIAN_DIAMOND_SIZE } from "../constants";
 import { DotPlotState, useChartState } from "../use-chart-state";
 
 export const DotPlotMedian = () => {
@@ -16,19 +15,7 @@ export const DotPlotMedian = () => {
     <>
       {yScale.domain().map((yValue) => {
         const y = (yScale(yValue) || 0) + yScale.bandwidth() / 2;
-        return (
-          <rect
-            key={yValue}
-            x={x - MEDIAN_DIAMOND_SIZE / 2}
-            y={y - MEDIAN_DIAMOND_SIZE / 2}
-            width={MEDIAN_DIAMOND_SIZE}
-            height={MEDIAN_DIAMOND_SIZE}
-            fill={palette.monochrome[800]}
-            stroke={palette.background.paper}
-            strokeWidth={1}
-            transform={`rotate(45, ${x}, ${y})`}
-          />
-        );
+        return <MedianDiamond x={x} y={y} yValue={yValue} />;
       })}
     </>
   );
