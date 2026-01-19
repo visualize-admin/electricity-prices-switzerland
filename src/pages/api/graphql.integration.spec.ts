a358b945667f75d8646a09c3f972d5121cb32855
Patrick Browne <pt.browne@gmail.com>
2026-01-19 09:42:24 +0100

    fix: Update tests

--- src/pages/api/graphql.integration.spec.ts
+++ src/pages/api/graphql.integration.spec.ts
@@ -162,7 +162,7 @@ describe("GraphQL API Integration Tests", () => {
                 },
               ],
             },
-            "sunshineMedianByIndicator": 15626.765,
+            "sunshineMedianByIndicator": 15650.214,
           },
         }
       `);
@@ -212,7 +212,7 @@ describe("GraphQL API Integration Tests", () => {
                 },
               ],
             },
-            "sunshineMedianByIndicator": 15626.765,
+            "sunshineMedianByIndicator": 15650.214,
           },
         }
       `);
